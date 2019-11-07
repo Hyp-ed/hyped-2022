e3916b591f9c8020a1920979713055ce4b674d2a
freddiejbawden <freddiejbawden@gmail.com>
2019-11-07 18:52:42 +0000

    Move and Remvoe GTest

--- test/setup.sh
+++ test/setup.sh
@@ -1,5 +1,6 @@
 cd lib && mkdir -p googletest
 tar -xf gtest.tar.gz -C googletest --strip-components 1
 cd googletest && mkdir -p build
+# change to g++
 cd build && cmake -DCMAKE_CXX_COMPILER="c++" -DCMAKE_CXX_FLAGS="-std=c++11 -stdlib=libc++"  ..
 make

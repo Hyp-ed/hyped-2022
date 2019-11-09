70643f9b7d07b1a2a339d22ac453e671541449b7
freddiejbawden <freddiejbawden@gmail.com>
2019-11-09 11:20:35 +0000

    config assume windows

--- utils/build/config.mk
+++ utils/build/config.mk
@@ -1,3 +1,10 @@
 CFLAGS:=-pthread -O2 -Wall -Wno-unused-result 
 CC:="g++"
 UNAME=$(shell uname)
+ifneq ($(UNAME),Linux)
+	# assume Windows
+	UNAME='Windows'
+	CFLAGS:=$(CFLAGS) -std=gnu++11
+else
+	CFLAGS:=$(CFLAGS) -std=c++11
+endif

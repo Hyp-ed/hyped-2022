bdd034fe7e6978df0721e1cdc7678a8197417c2d
freddiejbawden <freddiejbawden@gmail.com>
2019-11-09 11:10:21 +0000

    Clean Up Googletest

--- test/setup.sh
+++ test/setup.sh
@@ -3,12 +3,12 @@
 cd lib && mkdir -p googletest
 tar -xf googletest.tar.gz -C googletest --strip-components 1
 cd googletest && mkdir -p build
-UNAME="Windows"
+UNAME=$(uname)
 COMPILER=""
-if [$UNAME = "Windows"]; then 
+if [ $UNAME == "Windows" ]; then 
   COMPILER="gnu"
 else
    COMPILER="c"
 fi
-cd build && cmake -DCMAKE_CXX_COMPILER="g++" -DCMAKE_CXX_FLAGS="-std=$COMPILER++11 ..
+cd build && cmake -DCMAKE_CXX_COMPILER="g++" -DCMAKE_CXX_FLAGS="-std=$COMPILER++11" ..
 make

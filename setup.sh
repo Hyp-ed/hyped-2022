c0aa7bfd9de0094fd5fec3ab3ee8a8ecaa8e09b1
Martin Kristien <martin.kristien@gmail.com>
2019-11-18 16:28:44 +0000

    Add git hook to allow commits only when all tests are successful

--- setup.sh
+++ setup.sh
@@ -1,2 +1,17 @@
-# We do not longer use submodule for Eigen library
-# git submodule update --init --recursive --progress
+#!/bin/bash
+set -e
+
+echo -n "Deploying git hooks: "
+HOOKDIR=.git/hooks
+
+# remove old hooks
+rm -rf $HOOKDIR
+mkdir -p $HOOKDIR
+
+# copy new hooks, make executable
+cp utils/githooks/* $HOOKDIR/
+for file in .git/hooks/*
+do
+  chmod u+x $file
+done
+echo "SUCCESS"

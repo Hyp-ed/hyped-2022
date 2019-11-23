b90c093af6dec4bc1d876fb38da2f32acfb169c8
freddiejbawden <freddiejbawden@gmail.com>
2019-11-23 13:59:44 +0000

    Add Travis Integration

--- test/utils/get_code_cov.sh
+++ test/utils/get_code_cov.sh
@@ -21,6 +21,9 @@ lcov -q --directory bin/debug -b . --capture --no-external --output-file ./test/
 lcov -q --remove ./test/coverage/covdata/hyped.test.covdata `pwd`/lib/\* `pwd`/\*.test.cpp -o ./test/coverage/covdata/hyped.test.covdata
 echo "Combining Coverage Stats"
 lcov -q -a ./test/coverage/covdata/hyped.base.covdata -a ./test/coverage/covdata/hyped.test.covdata -o ./test/coverage/covdata/hyped.covdata
-echo "Generating HTML Report"s
-genhtml --output-directory ./test/coverage/coverage_html ./test/coverage/covdata/hyped.covdata --quiet
-echo "HTML Report saved at test/coverage/coverage_html"
+if [ -z $CI ];
+then
+  echo "Generating HTML Report"
+  genhtml --output-directory ./test/coverage/coverage_html ./test/coverage/covdata/hyped.covdata --quiet
+  echo "HTML Report saved at test/coverage/coverage_html"
+fi

66ecc1ba4ac31a8834f9b6e338cfa02b843d4ec9
Thanakrit-Anutrakulchai <tuchapoltr@gmail.com>
2019-11-26 13:53:30 +0000

    Add cppcheck on travis osx

--- test/lib/cppchecksetup.sh
+++ test/lib/cppchecksetup.sh
@@ -3,13 +3,11 @@
 
 if [ "$1" = "CI" ]
 then
-    echo "If branch: CI = $1" # TEMP DEBUG
     command -v cppcheck > /dev/null 2>&1 || {
         echo "Error: CPPCheck is not installed on Travis"
         exit 1
     }
-else 
-    echo "Else branch: CI = $1" # TEMP DEBUG
+else
     mkdir -p lib/cppcheck
     tar -xf lib/cppcheck.tar.gz -C lib/cppcheck --strip-components 1
     

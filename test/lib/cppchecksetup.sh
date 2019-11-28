c211e3e021bb969361cdb974f885b3a2223f121d
Thanakrit-Anutrakulchai <tuchapoltr@gmail.com>
2019-11-28 16:30:22 +0000

    Stop untar cppcheck if already present and simplify test/makefile

--- test/lib/cppchecksetup.sh
+++ test/lib/cppchecksetup.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 
-if [ "$1" = "CI" ]
+command -v cppcheck > /dev/null 2>&1
+if [ $? -eq 1 ]
 then
-    command -v cppcheck > /dev/null 2>&1 || {
+    if [ "$1" = "CI" ]
+    then 
         echo "Error: CPPCheck is not installed on Travis"
         exit 1
-    }
-else
+    fi
+    
+    echo "CPPCheck not found, unpacking from tar ..."
     mkdir -p lib/cppcheck
     tar -xf lib/cppcheck.tar.gz -C lib/cppcheck --strip-components 1
-    
+
     cd lib/cppcheck 
+    echo "Building CPPCheck Analyzer"
+	echo "This might take a while..."
     make MATCHCOMPILER=yes FILESDIR=./ HAVE_RULES=yes CXXFLAGS="-O2 -DNDEBUG -Wall -Wno-sign-compare -Wno-unused-function" 1> /dev/null
 fi

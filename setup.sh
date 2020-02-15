451a369e55414632981a319f86a4850ae42ccc6e
Paul Martin <paul@blibspace.com>
2020-02-15 14:19:57 +0000

    Fix setup.sh not working when hyped-2020 is a subrepo
    When hyped-2020 is cloned as a subrepo, .git is not a folder but a file pointing to the git-folder, which is located elsewhere

--- setup.sh
+++ setup.sh
@@ -2,7 +2,15 @@
 set -e
 
 echo -n "Deploying git hooks: "
+
 HOOKDIR=.git/hooks
+
+# when repo is a subrepo, .git is a file describing the path to the actual gitdir
+if [ -f .git ]
+then
+    # reroute $HOOKDIR to the actual directory
+    HOOKDIR=$(perl -nle 'print $1 if /gitdir: (.+)/' .git)
+fi
 
 # remove old hooks
 rm -rf $HOOKDIR
@@ -10,7 +18,7 @@ mkdir -p $HOOKDIR
 
 # copy new hooks, make executable
 cp utils/githooks/* $HOOKDIR/
-for file in .git/hooks/*
+for file in $HOOKDIR/*
 do
   chmod u+x $file
 done

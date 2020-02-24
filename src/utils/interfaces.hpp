2eddb9e99e8b14f7364da99dffdb908978606994
Iain Macpherson <iainmacpherson2@gmail.com>
2020-02-24 21:33:23 +0000

    Update comments and rename macro

--- src/utils/interfaces.hpp
+++ src/utils/interfaces.hpp
@@ -34,11 +34,11 @@
  * class name, not the full namespaced name.
  */
 
-// use format V(module/namespace, class)
-// e.g.       V(utils::config::fancy::typed::here, MyFancyInterface)
-#define INTERFACE_LIST(Register) \
-  Register(sensors, ImuInterface) \
-  Register(demo, DemoInterface)
+// use format REGISTER(module/namespace, class)
+// e.g.       REGISTER(utils::config::fancy::typed::here, MyFancyInterface)
+#define INTERFACE_LIST(REGISTER) \
+  REGISTER(sensors, ImuInterface) \
+  REGISTER(demo, DemoInterface)
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////

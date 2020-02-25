dc33779368c875e764cae6620e33ec8dc6557b17
Thanakrit-Anutrakulchai <tuchapoltr@gmail.com>
2020-02-25 23:43:25 +0000

    Fix cppcheck uncannot untar

--- test/lib/cppchecksetup.sh
+++ test/lib/cppchecksetup.sh
@@ -5,10 +5,10 @@ command -v cppcheck > /dev/null 2>&1
 if [ $? -eq 1 ]
 then
     echo "CPPCheck not found, unpacking from tar ..."
-    mkdir -p lib/cppcheck
-    tar -xf lib/cppcheck.tar.gz -C lib/cppcheck --strip-components 1
+    mkdir -p test/lib/cppcheck
+    tar -xf test/lib/cppcheck.tar.gz -C test/lib/cppcheck --strip-components 1
 
-    cd lib/cppcheck 
+    cd test/lib/cppcheck 
     echo "Building CPPCheck Analyzer"
 	echo "This might take a while..."
     make MATCHCOMPILER=yes FILESDIR=./ HAVE_RULES=yes CXXFLAGS="-O2 -DNDEBUG -Wall -Wno-sign-compare -Wno-unused-function" 1> /dev/null

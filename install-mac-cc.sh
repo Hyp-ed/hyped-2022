734f8144338b54d44032be00a2cfda6db6472d29
Neil Weidinger <neilweidingern@gmail.com>
2020-02-28 15:01:06 +0000

    Make mac crosscompiler installation script extract tarball into correct directory

--- install-mac-cc.sh
+++ install-mac-cc.sh
@@ -2,7 +2,7 @@
 set -e
 
 while true; do
-    read -p "Downloading the files will take a bit, make sure you have a fast internet connection. Continue? (yes/no) " yn
+    read -p $'Downloading the files will take a bit, make sure you have a decent internet connection.\nContinue?\n(yes/no) ' yn
     case $yn in
         [Yy]* ) break;;
         [Nn]* ) exit;;
@@ -13,7 +13,13 @@ done
 echo "Downloading mac crosscompiler files..."
 curl -L -O https://github.com/Hyp-ed/mac-crosscompiler/releases/download/v1.0/mac-crosscompiler.tar.gz
 
+echo "Making new mac crosscompiler directory..."
+mkdir mac-crosscompiler
+
 echo "Extracting mac crosscompiler archive..."
-tar xvf mac-crosscompiler.tar.gz
+tar --strip-components 1 -C mac-crosscompiler -xpvf mac-crosscompiler.tar.gz
+
+echo "Removing mac crosscompiler tarball..."
+rm -f mac-crosscompiler.tar.gz
 
 echo "Finished installing mac crosscompiler!"

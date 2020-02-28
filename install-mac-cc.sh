c65d40ca35de3285eb259271501eb23cd58ec4b5
Neil Weidinger <neilweidingern@gmail.com>
2020-02-28 18:18:20 +0000

    Make mac crosscompiler script a bit more readable

--- install-mac-cc.sh
+++ install-mac-cc.sh
@@ -2,21 +2,21 @@
 set -e
 
 while true; do
-    read -p $'Downloading the files will take a bit, make sure you have a decent internet connection.\nContinue?\n(yes/no) ' yn
-    case $yn in
+    read -p $'Downloading the files will take a bit, make sure you have a decent internet connection.\nContinue?\n(yes/no) ' answer
+    case $answer in
         [Yy]* ) break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
 done
 
-echo "Downloading mac crosscompiler files..."
+echo "Downloading mac crosscompiler tarball..."
 curl -L -O https://github.com/Hyp-ed/mac-crosscompiler/releases/download/v1.0/mac-crosscompiler.tar.gz
 
 echo "Making new mac crosscompiler directory..."
 mkdir mac-crosscompiler
 
-echo "Extracting mac crosscompiler archive..."
+echo "Extracting mac crosscompiler tarball..."
 tar --strip-components 1 -C mac-crosscompiler -xpvf mac-crosscompiler.tar.gz
 
 echo "Removing mac crosscompiler tarball..."

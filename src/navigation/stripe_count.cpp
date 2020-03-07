f8afcd24fd9b1748d6e39a30fd45e7a5f352b9b3
Justus Rudolph <justus.katte@gmail.com>
2020-03-07 12:32:37 +0000

    Add new StripeCount class. Not yet integrated with navigation

---
+++ src/navigation/stripe_count.cpp
@@ -0,0 +1,116 @@
+/*
+ * Author:Justus Rudolph
+ * Organisation: HYPED
+ * Date: 07/03/2020
+ * Description: Stripe counter class for navigation.
+ *
+ *    Copyright 2019 HYPED
+ *    Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ *    except in compliance with the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software distributed under
+ *    the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ *    either express or implied. See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include "navigation/stripe_count.hpp"
+
+namespace hyped {
+namespace navigation{
+
+StripeCount::StripeCount(Logger& log, Data& data, uint32_t init_time,
+      NavigationType& displ_unc, NavigationType& vel_unc, NavigationType stripe_dist)
+  : log_(log),
+    data_(data),
+    init_time_(init_time_),
+    displ_unc_(displ_unc),
+    vel_unc_(vel_unc),
+    stripe_counter_(0, 0),
+    failure_counter_(0),
+    stripe_dist_(stripe_dist)
+    {
+      prev_readings_ = data_.getSensorsKeyenceData();
+    }
+
+void StripeCount::getReadings()
+{
+  readings_ = data_.getSensorsKeyenceData();
+}
+
+void StripeCount::updateReadings()
+{
+  prev_readings_ = readings_;
+}
+
+bool StripeCount::checkFailure(NavigationType displ)
+{
+  // Failure if more than one disagreement
+  if (failure_counter_ > 1) {
+    log_.ERR("NAV", "More than one large IMU/Keyence disagreement, entering kCriticalFailure");
+    return true;
+  }
+  if (displ - stripe_counter_.value*stripe_dist_ > 4 * stripe_dist_) {
+    log_.ERR("NAV", "IMU distance at least 3 * stripe_dist_ ahead, entering kCriticalFailure.");
+    return true;
+  }
+  return false;
+}
+
+void StripeCount::updateNavData(NavigationType& displ, NavigationType& vel)
+{
+  NavigationType displ_change = displ - stripe_counter_.value*stripe_dist_;
+  vel -= displ_change*1e6/(stripe_counter_.timestamp - init_time_);
+  displ -= displ_change;
+}
+
+void StripeCount::queryKeyence(NavigationType& displ, NavigationType& vel, bool real)
+{
+  getReadings();
+
+  for (int i = 0; i < Sensors::kNumKeyence; i++) {
+    // Check new readings
+    if (prev_readings_[i].count.value != readings_[i].count.value &&
+         readings_[i].count.timestamp - stripe_counter_.timestamp > 1e5) {
+      stripe_counter_.value++;
+      stripe_counter_.timestamp = readings_[i].count.timestamp;
+      if (!real) stripe_counter_.timestamp = utils::Timer::getTimeMicros();
+
+      NavigationType allowed_uncertainty = displ_unc_;
+      NavigationType minimum_uncertainty = stripe_dist_ / 5.;
+
+      if (displ_unc_ < minimum_uncertainty) allowed_uncertainty = minimum_uncertainty;
+      NavigationType displ_change = displ - stripe_counter_.value*stripe_dist_;
+
+      // Allow up to one missed stripe
+      if (displ_change > stripe_dist_ - allowed_uncertainty &&
+          displ_change < stripe_dist_ + allowed_uncertainty &&
+          displ > stripe_counter_.value*stripe_dist_ + 0.5*stripe_dist_) {
+        stripe_counter_.value++;
+        displ_change -= stripe_dist_;
+      }
+      // Too large disagreement
+      if ((displ_change < (-2) * allowed_uncertainty) ||
+          (displ_change > 2 * allowed_uncertainty))
+      {
+        failure_counter_++;
+        failure_counter_ += floor(abs(displ_change) / stripe_dist_);
+      }
+      // Lower the uncertainty in velocity
+      vel_unc_ -= abs(displ_change*1e6/(stripe_counter_.timestamp - init_time_));
+      log_.DBG("NAV", "Stripe detected!");
+      log_.DBG1("NAV", "Timestamp difference: %d", stripe_counter_.timestamp - init_time_);
+      log_.DBG1("NAV", "Timestamp currently:  %d", stripe_counter_.timestamp);
+
+      // Ensure velocity uncertainty is positive
+      vel_unc_ = abs(vel_unc_);
+      break;
+    }
+  }
+  updateNavData(displ, vel);
+  // Update old keyence readings with current ones
+  updateReadings();
+}
+}}
---
+++ src/navigation/stripe_count.hpp
@@ -0,0 +1,120 @@
+/*
+ * Author:Justus Rudolph
+ * Organisation: HYPED
+ * Date: 07/03/2020
+ * Description: Stripe counter class for navigation.
+ *
+ *    Copyright 2019 HYPED
+ *    Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ *    except in compliance with the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software distributed under
+ *    the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ *    either express or implied. See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#ifndef NAVIGATION_STRIPECOUNT_HPP
+#define NAVIGATION_STRIPECOUNT_HPP
+
+#include <math.h>
+#include <array>
+#include <cstdint>
+
+#include "data/data.hpp"
+#include "data/data_point.hpp"
+#include "utils/logger.hpp"
+#include "utils/timer.hpp"
+#include "utils/math/statistics.hpp"
+
+namespace hyped {
+
+using data::Data;
+using data::DataPoint;
+using data::ModuleStatus;
+using data::NavigationType;
+using data::NavigationVector;
+using data::Motors;
+using data::Sensors;
+using utils::Logger;
+using std::array;
+
+namespace navigation {
+
+  class StripeCount {
+    public:
+      typedef array<data::StripeCounter, Sensors::kNumKeyence> KeyenceDataArray;
+
+      /**
+       * @brief Construct a new Stripe Counter object
+       *
+       * @param log System logger
+       * @param data Central data struct
+       * @param init_time Initial timestamp (for comparison)
+       * @param displ_unc Reference to uncertainty in displacement
+       * @param vel_unc Reference to uncertainty in velocity
+       */
+      explicit StripeCount(Logger& log, Data& data, uint32_t init_time,
+      NavigationType& displ_unc, NavigationType& vel_unc, NavigationType stripe_dist);
+
+      /**
+       * @brief Check if stripe has been detected
+       * 
+       * @param displ Current displacement
+       * @param vel Current velocity
+       * @param real Whether or not the sensors are real
+       */
+      void queryKeyence(NavigationType& displ, NavigationType& vel, bool real);
+      /**
+       * @brief Check if submodule should enter kCriticalFailure
+       * 
+       * @param displ Current displacement for comparison
+       * 
+       * @return bool to enter kCriticalFailure or not
+       */
+      bool checkFailure(NavigationType displ);
+      /**
+       * @brief Update nav data
+       *  
+       * @param displ Current displacement
+       * @param vel Current velocity
+       */
+      void updateNavData(NavigationType& displ, NavigationType& vel);
+
+    private:
+      // Central logging and data struct
+      Logger& log_;
+      Data& data_;
+
+      // Number of stripes hit & most recent timestamp
+      DataPoint<uint16_t> stripe_counter_;
+      // Keyence data read
+      KeyenceDataArray readings_;
+      // Previous keyence data for comparison
+      KeyenceDataArray prev_readings_;
+      // Number of significant sensor disagreements
+      uint8_t failure_counter_;
+      // Distance between stripes
+      NavigationType stripe_dist_;
+
+      // initial timestamp
+      uint32_t init_time_;
+      // displacement uncertainty
+      NavigationType& displ_unc_;
+      // velocity uncertainty
+      NavigationType& vel_unc_;
+
+      /**
+       * @brief update prev_readings
+       */
+      void updateReadings();
+      /**
+       * @brief get current readings
+       */
+      void getReadings();
+
+  };
+}}
+#endif // NAVIGATION_STRIPECOUNT_HPP

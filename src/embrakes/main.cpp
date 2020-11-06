3f061caa20e64fe3841b7992f6569c725a7aa32e
Atte Niemi <4998544+hur@users.noreply.github.com>
2020-11-06 16:45:38 +0000

    Remove unnecessary code from stepper

--- src/embrakes/main.cpp
+++ src/embrakes/main.cpp
@@ -59,135 +59,56 @@ void Main::run()
       case data::State::kIdle:
         if (!tlm_data_.nominal_braking_command) {
           if (brake_1->checkClamped()) {
-            log_.INFO("Brakes", "RETRACT COMMAND");
+            log_.INFO("Brakes", "Sending retract command");
             brake_1->sendRetract();
           }
-          // if(brake_2->checkClamped()){
-          //   brake_2->sendRetract();
-          // }
-          // if(brake_3->checkClamped){
-          //   brake_3->sendRetract();
-          // }
-          // if(brake_4->checkClamped){
-          //   brake_4->sendRetract();
-          // }
           Thread::sleep(1000);
           brake_1->checkHome();
-          // brake_2->checkHome();
-          // brake_3->checkHome();
-          // brake_4->checkHome();
-
         } else if (tlm_data_.nominal_braking_command) {
           if (!brake_1->checkClamped()) {
-            log_.INFO("Brakes", "ENGAGE COMMAND");
+            log_.INFO("Brakes", "Sending engage command");
             brake_1->sendClamp();
           }
-          // if(!brake_2->checkClamped()){
-          //   brake_2->sendClamp();
-          // }
-          // if(!brake_3->checkClamped){
-          //   brake_3->sendClamp();
-          // }
-          // if(!brake_4->checkClamped){
-          //   brake_4->sendClamp();
-          // }
           Thread::sleep(1000);
           brake_1->checkHome();
-          // brake_2->checkHome();
-          // brake_3->checkHome();
-          // brake_4->checkHome();
         }
         break;
       case data::State::kCalibrating:
         if (brake_1->checkClamped()) {
           brake_1->sendRetract();
         }
-        // if(brake_2->checkClamped()){
-        //   brake_2->sendRetract();
-        // }
-        // if(brake_3->checkClamped()){
-        //   brake_3->sendRetract();
-        // }
-        // if(brake_4->checkClamped()){
-        //   brake_4->sendRetract();
-        // }
         if (!brake_1->checkClamped()) {
           em_brakes_.module_status = ModuleStatus::kReady;
           data_.setEmergencyBrakesData(em_brakes_);
         }
         Thread::sleep(1000);
         brake_1->checkHome();
-        // brake_2->checkHome();
-        // brake_3->checkHome();
-        // brake_4->checkHome();
         break;
       case data::State::kAccelerating:
         brake_1->checkAccFailure();
-        // brake_2->checkAccFailure();
-        // brake_3->checkAccFailure();
-        // brake_4->checkAccFailure();
         break;
       case data::State::kNominalBraking:
         if (!brake_1->checkClamped()) {
           brake_1->sendClamp();
         }
-        // if(!brake_2->checkClamped()){
-        //   brake_2->sendClamp();
-        // }
-        // if(!brake_3->checkClamped()){
-        //   brake_3->sendClamp();
-        // }
-        // if(!brake_4->checkClamped()){
-        //   brake_4->sendClamp();
-        // }
         Thread::sleep(1000);
         brake_1->checkHome();
-        // brake_2->checkHome();
-        // brake_3->checkHome();
-        // brake_4->checkHome();
 
         brake_1->checkBrakingFailure();
-        // brake_2->checkBrakingFailure();
-        // brake_3->checkBrakingFailure();
-        // brake_4->checkBrakingFailure();
         break;
       case data::State::kFinished:
         if (tlm_data_.nominal_braking_command) {
           if (brake_1->checkClamped()) {
             brake_1->sendRetract();
           }
-          // if(brake_2->checkClamped()){
-          //   brake_2->sendRetract();
-          // }
-          // if(brake_3->checkClamped){
-          //   brake_3->sendRetract();
-          // }
-          // if(brake_4->checkClamped){
-          //   brake_4->sendRetract();
-          // }
           Thread::sleep(1000);
           brake_1->checkHome();
-          // brake_2->checkHome();
-          // brake_3->checkHome();
-          // brake_4->checkHome();
         } else if (!tlm_data_.nominal_braking_command) {
           if (!brake_1->checkClamped()) {
             brake_1->sendClamp();
           }
-          // if(!brake_2->checkClamped()){
-          //   brake_2->sendClamp();
-          // }
-          // if(!brake_3->checkClamped){
-          //   brake_3->sendClamp();
-          // }
-          // if(!brake_4->checkClamped){
-          //   brake_4->sendClamp();
-          // }
           Thread::sleep(500);
           brake_1->checkHome();
-          // brake_2->checkHome();
-          // brake_3->checkHome();
-          // brake_4->checkHome();
         }
         break;
       default:

f1f6867ba76c59bb35642b0c01adf91b4af97335
Cal Alexander <callumalexander.personal@gmail.com>
2021-01-25 18:18:47 +0000

    Initial branch commit

---
+++ test/src/utils/quaternion.test.cpp
@@ -0,0 +1,35 @@
+/*
+ * Author: Cal Alexander
+ * Organisation: HYPED
+ * Date: 24/01/2021
+ * Description: Testing file for quaternion.hpp
+ *
+ *    Copyright 2021 HYPED
+ *    Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ *    except in compliance with the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software distributed under
+ *    the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ *    either express or implied. See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+
+#include <iostream>
+#include "math.h"
+#include "gtest/gtest.h"
+#include "utils/math/quaternion.hpp"
+#include "utils/system.hpp"
+#include "data/data_point.hpp"
+
+namespace hyped {
+namespace utils {
+namespace math {
+
+// -------------------------------------------------------------------------------------------------
+// Helper Functions
+// -------------------------------------------------------------------------------------------------
+
+}}}

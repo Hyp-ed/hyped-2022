6ec855d7ab39d904d737e2ff61145323a56e2148
Franz Miltz <franz@miltz.me>
2021-02-13 20:32:16 +0000

    MTC: Fixed main loop logic

--- src/propulsion/main.hpp
+++ src/propulsion/main.hpp
@@ -19,49 +19,51 @@
 #ifndef PROPULSION_MAIN_HPP_
 #define PROPULSION_MAIN_HPP_
 
-#include "utils/concurrent/thread.hpp"
 #include "utils/concurrent/barrier.hpp"
+#include "utils/concurrent/thread.hpp"
+#include "utils/logger.hpp"
 #include "utils/system.hpp"
 #include "utils/timer.hpp"
-#include "utils/logger.hpp"
 // #include "propulsion/can/can_sender.hpp"
 #include "data/data.hpp"
-
 #include "state_processor.hpp"
 
-namespace hyped
-{
+namespace hyped {
 using data::Data;
-using data::State;
 using data::ModuleStatus;
 using data::Motors;
+using data::State;
 using data::Telemetry;
 using utils::Logger;
 using utils::System;
 using utils::concurrent::Thread;
 
-namespace motor_control
-{
+namespace motor_control {
 
 constexpr int32_t kNumMotors = 4;
 
-class Main : public Thread
-{
-    public:
-        Main(uint8_t id, Logger &log);
+class Main : public Thread {
+ public:
+  Main(uint8_t id, Logger &log);
 
-    /**
-     * @brief {This function is the entrypoint to the propulsion module and reacts to the certain states}
-    * */
-    void run() override;
+  /**
+   * @brief {This function is the entrypoint to the propulsion module and reacts to the certain
+   * states}
+   */
+  void run() override;
 
-  private:
-    bool is_running_;
-    Logger &log_;
-    StateProcessor *state_processor_;
-    State current_state_;
-    State previous_state_;
-    Data& data_;
+ private:
+  bool is_running_;
+  Logger &log_;
+  StateProcessor *state_processor_;
+  State current_state_;
+  State previous_state_;
+  /**
+   * @brief   Returns true iff the pod state has changed since the last check.
+   */
+  bool handleTransition();
+
+  void handleCriticalFailure(Data &data, Motors &motor_data);
 };
 
 }  // namespace motor_control

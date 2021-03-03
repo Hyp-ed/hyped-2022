b45e00601f879f54444cc095f0920360b0d574ae
Franz Miltz <franz@miltz.me>
2021-03-03 10:32:42 +0000

    NAV: Setting module status after initialisation

--- src/navigation/main.cpp
+++ src/navigation/main.cpp
@@ -41,6 +41,11 @@ namespace navigation {
     if (sys_.fake_keyence) nav_.Navigation::setKeyenceFake();
     if (sys_.enable_nav_write) nav_.logWrite();
 
+    // Setting module status for STM transition
+    data::Navigation nav_data = data.getNavigationData();
+    nav_data.module_status = ModuleStatus::kReady;
+    data.setNavigationData(nav_data);
+
     // wait for calibration state for calibration
     while (sys_.running_ && !navigation_complete) {
       State current_state = data.getStateMachineData().current_state;

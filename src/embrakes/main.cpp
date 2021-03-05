aee9fb654f9203d8cdc1f4b596e33904a5df03e2
Franz Miltz <franz@miltz.me>
2021-03-05 09:47:25 +0000

    SNS: Merged brakes behaviour for kNominalBraking and kEmergencyBraking

--- src/embrakes/main.cpp
+++ src/embrakes/main.cpp
@@ -109,19 +109,6 @@ void Main::run()
         f_brake_->checkAccFailure();
         break;
       case data::State::kNominalBraking:
-        if (!m_brake_->checkClamped()) {
-          m_brake_->sendClamp();
-        }
-        if (!f_brake_->checkClamped()) {
-          f_brake_->sendClamp();
-        }
-        Thread::sleep(em_brakes_.brake_command_wait_time);
-        m_brake_->checkHome();
-        f_brake_->checkHome();
-
-        m_brake_->checkBrakingFailure();
-        f_brake_->checkBrakingFailure();
-        break;
       case data::State::kEmergencyBraking:
         if (!m_brake_->checkClamped()) {
           m_brake_->sendClamp();

6f12a4742c711a56a58dbdfc0ef79d4a8e40feaa
Franz Miltz <franz@miltz.me>
2021-06-21 15:58:24 +0100

    Add system intialisation to test startup

--- test/src/main.test.cpp
+++ test/src/main.test.cpp
@@ -18,9 +18,17 @@
 
 #include <iostream>
 #include "gtest/gtest.h"
+#include "utils/system.hpp"
 
 int main(int argc, char** argv)
 {
+  // Initialising single system instance to be used across all tests
+  // construct argument list to pass to System::parseArgs
+  static char arg0[] = "testing.cpp";
+  static char arg1[] = "--config=test/config.txt";
+  char* args[] = {arg0, arg1};
+  hyped::utils::System::parseArgs(2, args);
+
   std::cout << "running\n";
   ::testing::InitGoogleTest(&argc, argv);
 

b678698fe2827b108ba79a8281bd17daa69ea6b5
Franz Miltz <franz@miltz.me>
2021-08-04 21:30:42 +0200

    MGT: Add mock tests for embrakes and navigation

---
+++ test/src/embrakes/demo.test.cpp
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+
+#include <memory>
+#include <embrakes/main.hpp>
+#include <utils/concurrent/thread.hpp>
+#include <utils/logger.hpp>
+#include <utils/system.hpp>
+
+#include "../test.hpp"
+
+namespace hyped::testing {
+
+using namespace hyped;
+
+class EmbrakesDemoTest : public Test {
+};
+
+TEST_F(EmbrakesDemoTest, handlesSysNotRunning)
+{
+  utils::System &sys = utils::System::getSystem();
+  sys.running_       = false;
+  auto embrakes       = std::make_unique<embrakes::Main>(0, log_);
+  embrakes->start();
+  embrakes->join();
+}
+
+}  // namespace hyped::testing
---
+++ test/src/navigation/demo.test.cpp
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+
+#include <memory>
+#include <navigation/main.hpp>
+#include <utils/concurrent/thread.hpp>
+#include <utils/logger.hpp>
+#include <utils/system.hpp>
+
+#include "../test.hpp"
+
+namespace hyped::testing {
+
+using namespace hyped;
+
+class NavigationDemoTest : public Test {
+};
+
+TEST_F(NavigationDemoTest, handlesSysNotRunning)
+{
+  utils::System &sys = utils::System::getSystem();
+  sys.running_       = false;
+  auto navigation       = std::make_unique<navigation::Main>(0, log_);
+  navigation->start();
+  navigation->join();
+}
+
+}  // namespace hyped::testing

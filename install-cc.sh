4737e51d19b2a5ff39a03ad3fc0c8876825b0206
Franz Miltz <franz@miltz.me>
2021-10-13 20:49:03 +0100

    MGT: Improve cc installer output

--- install-cc.sh
+++ install-cc.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
-BUILD_DIR=.
+BUILD_DIR=$(readlink -f .)
 SYSTEM=`uname`
 
 if [ $SYSTEM = "Linux" ]; then
-    echo "Downloading Linux x86 to ARM cross compiler"
     URL="https://developer.arm.com/-/media/Files/downloads/gnu-a/10.3-2021.07/binrel/gcc-arm-10.3-2021.07-x86_64-arm-none-linux-gnueabihf.tar.xz"
     ARCHIVE="$BUILD_DIR/gcc-arm-10.3-2021.07-x86_64-arm-none-linux-gnueabihf.tar.xz"
     DIR="$BUILD_DIR/gcc-arm-10.3-2021.07-x86_64-arm-none-linux-gnueabihf"
-    wget -O $ARCHIVE $URL
+    printf "Downloading Linux x86 to ARM cross compiler to $BUILD_DIR..."
+    wget -O $ARCHIVE $URL 2> /dev/null
+    printf "DONE\nExtracting $ARCHIVE..."
     tar xf $ARCHIVE
+    printf "DONE\n"
 elif [ $SYSTEM = "Darwin" ]; then
     echo "Downloading Darwin x86 to ARM cross compiler"
 else

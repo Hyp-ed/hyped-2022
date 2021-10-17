c0c71d0ff34aa601bd8229ffdb851b901d37cfd4
TomLonergan03 <tomlonergan91@gmail.com>
2021-10-17 19:02:42 +0100

    Fixed variable names

--- src/propulsion/RPM_regulator.hpp
+++ src/propulsion/RPM_regulator.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #define MAX_RPM 6000
-#define MAX_TEMP 150
+#define MAX_TEMPERATURE 150
 #define MAX_CURRENT 1500  // mA
 
 #include "controller.hpp"
@@ -32,14 +32,14 @@ class RPM_Regulator {
    * @brief Calculate the optimal rpm based on criteria from all the motors
    *        as well optimal values produced by simulations.
    *
-   * @param act_velocity - the actual velocity of the pod from navigation
-   * @param act_rpm - average rpm of all the motors
-   * @param act_current - max current (mA) drawn out of all the motors
-   * @param act_temp - max temperature out of all the motors
+   * @param actual_velocity - the actual velocity of the pod from navigation
+   * @param actual_rpm - average rpm of all the motors
+   * @param actual_current - max current (mA) drawn out of all the motors
+   * @param actual_temperature - max temperature out of all the motors
    * @return int32_t - the optimal rpm which the motors should be set to.
    */
-  int32_t calculateRPM(int32_t act_velocity, int32_t act_rpm, int32_t act_current,
-                       int32_t act_temp);
+  int32_t calculateRPM(int32_t actual_velocity, int32_t actual_rpm, int32_t actual_current,
+                       int32_t actual_temperature);
 
   /**
    * @brief Get the Failure boolean
@@ -53,19 +53,19 @@ class RPM_Regulator {
   /**
    * @brief calculates the optimal rpm based off of the current velocity.
    *
-   * @param act_velocity
+   * @param actual_velocity
    * @return double - optimal rpm
    */
-  int32_t calculateOptimalRPM(int32_t act_velocity);
+  int32_t calculateOptimalRPM(int32_t actual_velocity);
 
   /**
    * @brief calculate the step to increase or decrease the rpm by.
    *
-   * @param opt_rpm - the optimal rpm for our current velocity
+   * @param optimal_rpm - the optimal rpm for our current velocity
    * @param direction - the direction that the step must go in: true for pos false for neg.
    * @return int32_t - the step with which to increase the rpm
    */
-  int32_t step(int32_t opt_rpm, bool direction);
+  int32_t step(int32_t optimal_rpm, bool direction);
 
   Logger &log_;
   int32_t current_index;

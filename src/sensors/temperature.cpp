82303f5a6a7a98a045de1dd55a8e1648b9b05690
Meghdeep Basu <meghdeep.basu17@gmail.com>
2021-10-21 18:17:06 +0100

    Changed the comments. Made conversion comment more specific

--- src/sensors/temperature.cpp
+++ src/sensors/temperature.cpp
@@ -30,11 +30,9 @@ void Temperature::run()
 
 int Temperature::scaleData(uint16_t raw_value)
 {
-  // convert to degrees C
-  // convert from what to degrees C?
+  // convert from Potential Difference to degrees C
   double temp = static_cast<double>(raw_value) / 4095;
-  temp        = (temp * 175) - 50;
-  // I have never seen this ^^ temperature calculation before
+  temp        = (temp * 200) - 50;
   return static_cast<int>(temp);
 }
 

94d42978986c7a774b7b7bb85b6cb1fd4e4346f0
Sashank <sashank17102002@yahoo.in>
2021-10-21 11:42:32 +0100

    Fixed types

--- src/telemetry/main.cpp
+++ src/telemetry/main.cpp
@@ -8,8 +8,8 @@ namespace hyped {
 
 namespace telemetry {
 
-Main::Main(const uint8_t id, Logger &log)
-    : Thread{id, log},
+Main::Main(const uint8_t id, utils::Logger &log)
+    : utils::concurrent::Thread{id, log},
       data_{data::Data::getInstance()},
       client_{log}
 {
@@ -19,7 +19,7 @@ Main::Main(const uint8_t id, Logger &log)
 void Main::run()
 {
   // check if telemetry is disabled
-  const auto &sys                = hyped::utils::System::getSystem();
+  const auto &sys                = utils::System::getSystem();
   data::Telemetry telemetry_data = data_.getTelemetryData();
 
   if (sys.telemetry_off) {

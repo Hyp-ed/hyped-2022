adb789c082f15fee32cd6893f92d1b73aa5b87b4
Tom Lonergan <tomlonergan91@gmail.com>
2021-10-23 14:42:41 +0100

    Changed if statement for clarity

--- src/propulsion/file_reader.cpp
+++ src/propulsion/file_reader.cpp
@@ -23,19 +23,19 @@ bool FileReader::readFileData(ControllerMessage *messages, const char *filepath)
 
   int m = 0;
   char line[250];
+
   while (fgets(line, static_cast<int>(sizeof(line) / sizeof(line[0])), current_file) != NULL) {
-    if (line[0] == '\n' || line[0] == '\0') {
-    } else if (line[0] == '#') {
-    } else if (line[0] == '>') {
+    if (line[0] == '\n' || line[0] == '\0' || line[0] == '#') { continue; }
+    if (line[0] == '>') {
       for (int i = 1; i < static_cast<int>(strlen(line)) - 1; i++) {
         messages[m].logger_output[i - 1] = line[i];
       }
-    } else {
-      std::string lineData[8];
-      splitData(line, lineData);
-      addData(lineData, messages[m].message_data);
-      m++;
+      continue;
     }
+    std::string line_data[8];
+    splitData(line, line_data);
+    addData(line_data, messages[m].message_data);
+    m++;
   }
 
   fclose(current_file);
@@ -51,14 +51,14 @@ void FileReader::splitData(const std::basic_string<char> line, std::string line_
     tokens.push_back(intermediate);
   }
   for (int i = 0; i < (signed)tokens.size(); i++) {
-    lineData[i] = tokens[i];
+    line_data[i] = tokens[i];
   }
 }
 
 void FileReader::addData(const std::string line_data[], uint8_t *message_data)
 {
   for (int i = 0; i < 8; ++i) {
-    message_data[i] = static_cast<uint8_t>(std::stoi(lineData[i], NULL, 16));
+    message_data[i] = static_cast<uint8_t>(std::stoi(line_data[i], NULL, 16));
   }
 }
 }  // namespace motor_control

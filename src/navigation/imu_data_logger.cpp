4e858d4c9dea26cad2d5ae8c7734e02a5375194a
callumTM <callummcwilliam@btinternet.com>
2021-10-23 18:26:02 +0100

    NAV: reformatted dataToFile methods and changed \n to std::endl

--- src/navigation/imu_data_logger.cpp
+++ src/navigation/imu_data_logger.cpp
@@ -18,7 +18,7 @@ void ImuDataLogger::setup(const int imu_id, const int run_id)
   snprintf(buff, sizeof(buff), "test_data/run%d/imu%d/data.csv", run_id, imu_id);
   file_path_ = buff;
   outfile_->open(file_path_);
-  *outfile_ << "arx,ary,arz,acx,acy,acz,t\n";
+  *outfile_ << "arx,ary,arz,acx,acy,acz,t" << std::endl;
 }
 
 void ImuDataLogger::setupKalman(const int imu_id, const int run_id)
@@ -27,31 +27,43 @@ void ImuDataLogger::setupKalman(const int imu_id, const int run_id)
   snprintf(buff, sizeof(buff), "test_data/run%d/imu%d/data.csv", run_id, imu_id);
   file_path_ = buff;
   outfile_->open(file_path_);
-  *outfile_ << "arx,ary,arz,acx,acy,acz,afx,afy,afz,t\n";
+  *outfile_ << "arx,ary,arz,acx,acy,acz,afx,afy,afz,t" << std::endl;
 }
 
 void ImuDataLogger::dataToFileSimulation(data::NavigationVector &acceleration, uint32_t timestamp)
 {
-  *outfile_ << acceleration[0] << "," << acceleration[1] << "," << acceleration[2] << ","
-            << timestamp << "\n";
+  *outfile_ << acceleration[0] << ",";
+  *outfile_ << acceleration[1] << ",";
+  *outfile_ << acceleration[2] << ",";
+  *outfile_ << timestamp << std::endl;
 }
 
 void ImuDataLogger::dataToFile(data::NavigationVector &raw_acceleration,
                                data::NavigationVector &calibrated_acceleration, uint32_t timestamp)
 {
-  *outfile_ << raw_acceleration[0] << "," << raw_acceleration[1] << "," << raw_acceleration[2]
-            << "," << calibrated_acceleration[0] << "," << calibrated_acceleration[1] << ","
-            << calibrated_acceleration[2] << "," << timestamp << "\n";
+  *outfile_ << raw_acceleration[0] << ",";
+  *outfile_ << raw_acceleration[1] << ",";
+  *outfile_ << raw_acceleration[2] << ",";
+  *outfile_ << calibrated_acceleration[0] << ",";
+  *outfile_ << calibrated_acceleration[1] << ",";
+  *outfile_ << calibrated_acceleration[2] << ",";
+  *outfile_ << timestamp << std::endl;
 }
 
 void ImuDataLogger::dataToFileKalman(data::NavigationVector &raw_acceleration,
                                      data::NavigationVector &calibrated_acceleration,
                                      data::NavigationVector &x, uint32_t timestamp)
 {
-  *outfile_ << raw_acceleration[0] << "," << raw_acceleration[1] << "," << raw_acceleration[2]
-            << "," << calibrated_acceleration[0] << "," << calibrated_acceleration[1] << ","
-            << calibrated_acceleration[2] << "," << x[0] << "," << x[1] << "," << x[2] << ","
-            << timestamp << "\n";
+  *outfile_ << raw_acceleration[0] << ",";
+  *outfile_ << raw_acceleration[1] << ",";
+  *outfile_ << raw_acceleration[2] << ",";
+  *outfile_ << calibrated_acceleration[0] << ",";
+  *outfile_ << calibrated_acceleration[1] << ",";
+  *outfile_ << calibrated_acceleration[2] << ",";
+  *outfile_ << x[0] << ",";
+  *outfile_ << x[1] << ",";
+  *outfile_ << x[2] << ",";
+  *outfile_ << timestamp << std::endl;
 }
 
 }  // namespace navigation

149f3659cfd042ac45dc4f60e38beb9f3b735cee
dhb03 <hb.Dorna@yahoo.co.uk>
2021-10-25 15:21:53 +0100

    Update recvloop

--- src/telemetry/recvloop.cpp
+++ src/telemetry/recvloop.cpp
@@ -4,13 +4,11 @@
 
 namespace hyped {
 
-using data::ModuleStatus;
-
 namespace telemetry {
 
-RecvLoop::RecvLoop(Logger &log, data::Data &data, Main *main_pointer)
-    : Thread{log},
-      main_ref_{*main_pointer},
+RecvLoop::RecvLoop(utils::Logger &log, data::Data &data, utils::Client &client)
+    : utils::concurrent::Thread{log},
+      client_{client},
       data_{data}
 {
   log_.DBG("Telemetry", "Telemetry RecvLoop thread object created");
@@ -18,55 +16,66 @@ RecvLoop::RecvLoop(Logger &log, data::Data &data, Main *main_pointer)
 
 void RecvLoop::run()
 {
-  log_.DBG("Telemetry", "Telemetry RecvLoop thread started");
-
-  data::Telemetry telem_data = data_.getTelemetryData();
-  std::string message;
-
   while (true) {
+    log_.DBG("Telemetry", "Telemetry RecvLoop thread started");
+
+    data::Telemetry telemetry_data = data_.getTelemetryData();
+    std::string message;
     try {
-      message = main_ref_.client_.receiveData();
+      message = client_.receiveData();
     } catch (std::exception &e) {
       log_.ERR("Telemetry", "%s", e.what());
 
-      telem_data.module_status = ModuleStatus::kCriticalFailure;
-      data_.setTelemetryData(telem_data);
+      telemetry_data.module_status = data::ModuleStatus::kCriticalFailure;
+      data_.setTelemetryData(telemetry_data);
 
       break;
     }
-
-    if (message == "ACK") {
-      log_.INFO("Telemetry", "FROM SERVER: ACK");
-    } else if (message == "STOP") {
-      log_.INFO("Telemetry", "FROM SERVER: STOP");
-      telem_data.emergency_stop_command = true;
-    } else if (message == "CALIBRATE") {
-      log_.INFO("Telemetry", "FROM SERVER: CALIBRATE");
-      telem_data.calibrate_command = true;
-    } else if (message == "LAUNCH") {
-      log_.INFO("Telemetry", "FROM SERVER: LAUNCH");
-      telem_data.launch_command = true;
-    } else if (message == "SHUTDOWN") {
-      log_.INFO("Telemetry", "FROM SERVER: SHUTDOWN");
-      telem_data.shutdown_command = true;
-    } else if (message == "SERVER_PROPULSION_GO") {
-      log_.INFO("Telemetry", "FROM SERVER: SERVICE_PROPULSION_GO");
-      telem_data.service_propulsion_go = true;
-    } else if (message == "SERVER_PROPULSION_STOP") {
-      log_.INFO("Telemetry", "FROM SERVER: SERVICE_PROPULSION_STOP");
-      telem_data.service_propulsion_go = false;
-    } else if (message == "NOMINAL_BRAKING") {
-      log_.INFO("Telemetry", "FROM SERVER: NOMINAL_BRAKING");
-      telem_data.nominal_braking_command = true;
-    } else if (message == "NOMINAL_RETRACT") {
-      log_.INFO("Telemetry", "FROM SERVER: NOMINAL_RETRACT");
-      telem_data.nominal_braking_command = false;
-    } else {
-      log_.ERR("Telemetry", "Unrecognized input from server, ENTERING CRITICAL FAILURE");
-      telem_data.module_status = ModuleStatus::kCriticalFailure;
+    switch (message){
+      case "ACK":
+        log_.INFO("Telemetry", "FROM SERVER: ACK");
+        break;
+      case "STOP":
+        log_.INFO("Telemetry", "FROM SERVER: STOP");
+        telemetry_data.emergency_stop_command = true;
+        break;
+      case "CALIBRATE":
+        log_.INFO("Telemetry", "FROM SERVER: CALIBRATE");
+        telemetry_data.calibrate_command = true;
+        break;
+      case "LAUNCH":
+        log_.INFO("Telemetry", "FROM SERVER: LAUNCH");
+        telemetry_data.launch_command = true;
+        break;
+      case "SHUTDOWN":
+        log_.INFO("Telemetry", "FROM SERVER: SHUTDOWN");
+        telemetry_data.shutdown_command = true;
+        break;
+      case "SERVER_PROPULSION_GO":
+        log_.INFO("Telemetry", "FROM SERVER: SERVICE_PROPULSION_GO");
+        telemetry_data.service_propulsion_go = true;
+        break;
+      case "SERVER_PROPULSION_STOP":
+        log_.INFO("Telemetry", "FROM SERVER: SERVICE_PROPULSION_STOP");
+        telemetry_data.service_propulsion_go = false;
+        break;
+      case "NOMINAL_BRAKING":
+        log_.INFO("Telemetry", "FROM SERVER: NOMINAL_BRAKING");
+        telemetry_data.nominal_braking_command = true;
+        break;
+      case "NOMINAL_RETRACT":
+        log_.INFO("Telemetry", "FROM SERVER: NOMINAL_RETRACT");
+        telemetry_data.nominal_braking_command = false;
+        break;
+      default:
+        log_.ERR("Telemetry", "Unrecognized input from server, ENTERING CRITICAL FAILURE");
+        telemetry_data.module_status = data::ModuleStatus::kCriticalFailure;
+    }
+    
+    } 
     }
 
-    data_.setTelemetryData(telem_data);
+    data_.setTelemetryData(telemetry_data);
   }
 
   log_.DBG("Telemetry", "Exiting Telemetry RecvLoop thread");

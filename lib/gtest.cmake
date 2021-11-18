2a060a87d6a9793bd6e59f6dfda148d10c0505bd
Franz Miltz <franz@miltz.me>
2021-11-18 09:44:42 +0000

    MGT: Fix GTest build

--- lib/gtest.cmake
+++ lib/gtest.cmake
@@ -4,6 +4,7 @@ ExternalProject_Add(
     googletest
     PREFIX "lib/gtest"
     GIT_REPOSITORY "https://github.com/google/googletest"
+    GIT_TAG "e2239ee6043f73722e7aa812a459f54a28552929" # release 1.11.0 
     TIMEOUT 10
     INSTALL_COMMAND ""
 )

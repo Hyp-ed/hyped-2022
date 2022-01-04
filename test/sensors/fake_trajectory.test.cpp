6a702cf95515d70d726ce7a1e2f43003f2b1e3d6
Franz Miltz <franz@miltz.me>
2022-01-04 16:57:16 +0100

    SNS: Fake trajectory acceleration tests

--- test/sensors/fake_trajectory.test.cpp
+++ test/sensors/fake_trajectory.test.cpp
@@ -6,9 +6,17 @@
 namespace hyped::testing {
 
 class FakeTrajectoryTest : public Test {
+ public:
+  const std::vector<data::State> pre_run_states_
+    = {data::State::kIdle, data::State::kPreCalibrating, data::State::kCalibrating,
+       data::State::kReady};
+  const std::vector<data::State> braking_states_
+    = {data::State::kCruising, data::State::kNominalBraking, data::State::kEmergencyBraking};
+  const std::vector<data::State> post_run_states_
+    = {data::State::kFailureStopped, data::State::kFinished};
 };
 
-TEST_F(FakeTrajectoryTest, parseConfig)
+TEST_F(FakeTrajectoryTest, parsesConfig)
 {
   const std::string path     = "configurations/fake_trajectory.json";
   const auto fake_trajectory = sensors::FakeTrajectory::fromFile(log_, path);
@@ -19,6 +27,77 @@ TEST_F(FakeTrajectoryTest, parseConfig)
   ASSERT_FLOAT_EQ(2.0, config.braking_deceleration);
   ASSERT_FLOAT_EQ(0.01, config.cruising_deceleration);
   disableOutput();
+}
+
+TEST_F(FakeTrajectoryTest, noMovementBeforeRun)
+{
+  const std::string path = "configurations/fake_trajectory.json";
+  auto fake_trajectory   = sensors::FakeTrajectory::fromFile(log_, path);
+  auto &data             = data::Data::getInstance();
+  for (const auto state : pre_run_states_) {
+    auto state_machine_data          = data.getStateMachineData();
+    state_machine_data.current_state = state;
+    data.setStateMachineData(state_machine_data);
+    const auto trajectory = fake_trajectory->getTrajectory();
+    enableOutput();
+    ASSERT_FLOAT_EQ(0.0, trajectory.acceleration);
+    ASSERT_FLOAT_EQ(0.0, trajectory.velocity);
+    ASSERT_FLOAT_EQ(0.0, trajectory.displacement);
+    disableOutput();
+  }
+}
+
+TEST_F(FakeTrajectoryTest, expectedAcclerations)
+{
+  const std::string path = "configurations/fake_trajectory.json";
+  auto fake_trajectory   = sensors::FakeTrajectory::fromFile(log_, path);
+  auto &data             = data::Data::getInstance();
+  // Pre-run
+  {
+    for (const auto state : pre_run_states_) {
+      auto state_machine_data          = data.getStateMachineData();
+      state_machine_data.current_state = state;
+      data.setStateMachineData(state_machine_data);
+      const auto trajectory = fake_trajectory->getTrajectory();
+      enableOutput();
+      ASSERT_FLOAT_EQ(trajectory.acceleration, 0.0);
+      disableOutput();
+    }
+  }
+  // Accelerating
+  {
+    auto state_machine_data          = data.getStateMachineData();
+    state_machine_data.current_state = data::State::kAccelerating;
+    data.setStateMachineData(state_machine_data);
+    const auto trajectory = fake_trajectory->getTrajectory();
+    enableOutput();
+    ASSERT_GT(trajectory.acceleration, 0.0);
+    disableOutput();
+  }
+  // Braking
+  {
+    for (const auto state : braking_states_) {
+      auto state_machine_data          = data.getStateMachineData();
+      state_machine_data.current_state = state;
+      data.setStateMachineData(state_machine_data);
+      const auto trajectory = fake_trajectory->getTrajectory();
+      enableOutput();
+      ASSERT_LT(trajectory.acceleration, 0.0);
+      disableOutput();
+    }
+  }
+  // Post-run
+  {
+    for (const auto state : post_run_states_) {
+      auto state_machine_data          = data.getStateMachineData();
+      state_machine_data.current_state = state;
+      data.setStateMachineData(state_machine_data);
+      const auto trajectory = fake_trajectory->getTrajectory();
+      enableOutput();
+      ASSERT_FLOAT_EQ(trajectory.acceleration, 0.0);
+      disableOutput();
+    }
+  }
 }
 
 }  // namespace hyped::testing

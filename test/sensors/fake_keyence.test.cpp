e674aaeed52cfd21a2f17675f6aac7bef305210e
Franz Miltz <franz@miltz.me>
2022-01-17 17:20:34 +0000

    SNS: Fake keyence two faulty tests and loop unwrapping

--- test/sensors/fake_keyence.test.cpp
+++ test/sensors/fake_keyence.test.cpp
@@ -11,10 +11,11 @@ class FakeKeyenceTest : public Test {
   static constexpr uint64_t kSleepMillis = 100;
   static constexpr size_t kNumIterations = 10;
   const std::string kOneFaultyConfigPath = "configurations/test/fake_keyence_one_faulty.json";
+  const std::string kTwoFaultyConfigPath = "configurations/test/fake_keyence_two_faulty.json";
 };
 
 //---------------------------------------------------------------------------
-// Default configuration (no failures, all operational)
+// Both operational
 //---------------------------------------------------------------------------
 
 TEST_F(FakeKeyenceTest, defaultParsesConfig)
@@ -28,8 +29,8 @@ TEST_F(FakeKeyenceTest, defaultParsesConfig)
   const auto fake_keyences = *fake_keyences_optional;
   ASSERT_EQ(data::Sensors::kNumKeyence, fake_keyences.size());
   ASSERT_FLOAT_EQ(0.1, fake_keyences.at(0).getConfig().noise);
-  ASSERT_FALSE(fake_keyences.at(0).getConfig().failure_in_state);
   ASSERT_FLOAT_EQ(0.2, fake_keyences.at(1).getConfig().noise);
+  ASSERT_FALSE(fake_keyences.at(0).getConfig().failure_in_state);
   ASSERT_FALSE(fake_keyences.at(1).getConfig().failure_in_state);
   disableOutput();
 }
@@ -55,20 +56,19 @@ TEST_F(FakeKeyenceTest, defaultNonDecreasingData)
     state_machine_data.current_state = data::State::kAccelerating;
     data.setStateMachineData(state_machine_data);
   }
-  std::array<data::CounterData, fake_keyences.size()> previous_counter_data;
-  for (size_t i = 0; i < fake_keyences.size(); ++i) {
-    previous_counter_data.at(i) = fake_keyences.at(i).getData();
-  }
+  std::array<data::CounterData, fake_keyences.size()> previous_counter_data
+    = {fake_keyences.at(0).getData(), fake_keyences.at(1).getData()};
   for (size_t i = 0; i < kNumIterations; ++i) {
     utils::concurrent::Thread::sleep(kSleepMillis);
-    for (size_t i = 0; i < fake_keyences.size(); ++i) {
-      const auto current_counter_data = fake_keyences.at(i).getData();
-      enableOutput();
-      ASSERT_TRUE(current_counter_data.operational);
-      ASSERT_GE(current_counter_data.value, previous_counter_data.at(i).value);
-      disableOutput();
-      previous_counter_data.at(i) = current_counter_data;
-    }
+    const std::array<data::CounterData, data::Sensors::kNumKeyence> current_counter_data
+      = {fake_keyences.at(0).getData(), fake_keyences.at(1).getData()};
+    enableOutput();
+    ASSERT_TRUE(current_counter_data.at(0).operational);
+    ASSERT_TRUE(current_counter_data.at(1).operational);
+    ASSERT_GE(current_counter_data.at(0).value, previous_counter_data.at(0).value);
+    ASSERT_GE(current_counter_data.at(1).value, previous_counter_data.at(1).value);
+    disableOutput();
+    previous_counter_data = current_counter_data;
   }
   {
     auto state_machine_data          = data.getStateMachineData();
@@ -77,14 +77,15 @@ TEST_F(FakeKeyenceTest, defaultNonDecreasingData)
   }
   for (size_t i = 0; i < kNumIterations; ++i) {
     utils::concurrent::Thread::sleep(kSleepMillis);
-    for (size_t i = 0; i < fake_keyences.size(); ++i) {
-      const auto current_counter_data = fake_keyences.at(i).getData();
-      enableOutput();
-      ASSERT_TRUE(current_counter_data.operational);
-      ASSERT_GE(current_counter_data.value, previous_counter_data.at(i).value);
-      disableOutput();
-      previous_counter_data.at(i) = current_counter_data;
-    }
+    const std::array<data::CounterData, data::Sensors::kNumKeyence> current_counter_data
+      = {fake_keyences.at(0).getData(), fake_keyences.at(1).getData()};
+    enableOutput();
+    ASSERT_TRUE(current_counter_data.at(0).operational);
+    ASSERT_TRUE(current_counter_data.at(1).operational);
+    ASSERT_GE(current_counter_data.at(0).value, previous_counter_data.at(0).value);
+    ASSERT_GE(current_counter_data.at(1).value, previous_counter_data.at(1).value);
+    disableOutput();
+    previous_counter_data = current_counter_data;
   }
   {
     auto state_machine_data          = data.getStateMachineData();
@@ -93,14 +94,15 @@ TEST_F(FakeKeyenceTest, defaultNonDecreasingData)
   }
   for (size_t i = 0; i < kNumIterations; ++i) {
     utils::concurrent::Thread::sleep(kSleepMillis);
-    for (size_t i = 0; i < fake_keyences.size(); ++i) {
-      const auto current_counter_data = fake_keyences.at(i).getData();
-      enableOutput();
-      ASSERT_TRUE(current_counter_data.operational);
-      ASSERT_GE(current_counter_data.value, previous_counter_data.at(i).value);
-      disableOutput();
-      previous_counter_data.at(i) = current_counter_data;
-    }
+    const std::array<data::CounterData, data::Sensors::kNumKeyence> current_counter_data
+      = {fake_keyences.at(0).getData(), fake_keyences.at(1).getData()};
+    enableOutput();
+    ASSERT_TRUE(current_counter_data.at(0).operational);
+    ASSERT_TRUE(current_counter_data.at(1).operational);
+    ASSERT_GE(current_counter_data.at(0).value, previous_counter_data.at(0).value);
+    ASSERT_GE(current_counter_data.at(1).value, previous_counter_data.at(1).value);
+    disableOutput();
+    previous_counter_data = current_counter_data;
   }
   {
     auto state_machine_data          = data.getStateMachineData();
@@ -109,19 +111,20 @@ TEST_F(FakeKeyenceTest, defaultNonDecreasingData)
   }
   for (size_t i = 0; i < kNumIterations; ++i) {
     utils::concurrent::Thread::sleep(kSleepMillis);
-    for (size_t i = 0; i < fake_keyences.size(); ++i) {
-      const auto current_counter_data = fake_keyences.at(i).getData();
-      enableOutput();
-      ASSERT_TRUE(current_counter_data.operational);
-      ASSERT_GE(current_counter_data.value, previous_counter_data.at(i).value);
-      disableOutput();
-      previous_counter_data.at(i) = current_counter_data;
-    }
+    const std::array<data::CounterData, data::Sensors::kNumKeyence> current_counter_data
+      = {fake_keyences.at(0).getData(), fake_keyences.at(1).getData()};
+    enableOutput();
+    ASSERT_TRUE(current_counter_data.at(0).operational);
+    ASSERT_TRUE(current_counter_data.at(1).operational);
+    ASSERT_GE(current_counter_data.at(0).value, previous_counter_data.at(0).value);
+    ASSERT_GE(current_counter_data.at(1).value, previous_counter_data.at(1).value);
+    disableOutput();
+    previous_counter_data = current_counter_data;
   }
 }
 
 //---------------------------------------------------------------------------
-// One failure
+// One faulty, one operational
 //---------------------------------------------------------------------------
 
 TEST_F(FakeKeyenceTest, oneFaultyParsesConfig)
@@ -135,12 +138,13 @@ TEST_F(FakeKeyenceTest, oneFaultyParsesConfig)
   const auto fake_keyences = *fake_keyences_optional;
   ASSERT_EQ(data::Sensors::kNumKeyence, fake_keyences.size());
   ASSERT_FLOAT_EQ(0.1, fake_keyences.at(0).getConfig().noise);
-  ASSERT_TRUE(fake_keyences.at(0).getConfig().failure_in_state);
-  ASSERT_EQ(data::State::kNominalBraking, *fake_keyences.at(0).getConfig().failure_in_state);
   ASSERT_FLOAT_EQ(0.2, fake_keyences.at(1).getConfig().noise);
+  ASSERT_TRUE(fake_keyences.at(0).getConfig().failure_in_state);
   ASSERT_FALSE(fake_keyences.at(1).getConfig().failure_in_state);
+  ASSERT_EQ(data::State::kNominalBraking, *fake_keyences.at(0).getConfig().failure_in_state);
   disableOutput();
 }
+
 TEST_F(FakeKeyenceTest, oneFaultyNonDecreasingData)
 {
   auto &data = data::Data::getInstance();
@@ -162,21 +166,19 @@ TEST_F(FakeKeyenceTest, oneFaultyNonDecreasingData)
     state_machine_data.current_state = data::State::kAccelerating;
     data.setStateMachineData(state_machine_data);
   }
-  std::array<data::CounterData, fake_keyences.size()> previous_counter_data;
-  for (size_t i = 0; i < fake_keyences.size(); ++i) {
-    previous_counter_data.at(i) = fake_keyences.at(i).getData();
-  }
+  std::array<data::CounterData, fake_keyences.size()> previous_counter_data
+    = {fake_keyences.at(0).getData(), fake_keyences.at(1).getData()};
   for (size_t i = 0; i < kNumIterations; ++i) {
     utils::concurrent::Thread::sleep(kSleepMillis);
-    for (size_t i = 0; i < fake_keyences.size(); ++i) {
-      const auto current_counter_data = fake_keyences.at(i).getData();
-      enableOutput();
-      if (current_counter_data.operational) {
-        ASSERT_GE(current_counter_data.value, previous_counter_data.at(i).value);
-      }
-      disableOutput();
-      previous_counter_data.at(i) = current_counter_data;
-    }
+    const std::array<data::CounterData, data::Sensors::kNumKeyence> current_counter_data
+      = {fake_keyences.at(0).getData(), fake_keyences.at(1).getData()};
+    enableOutput();
+    ASSERT_TRUE(current_counter_data.at(0).operational);
+    ASSERT_TRUE(current_counter_data.at(1).operational);
+    ASSERT_GE(current_counter_data.at(0).value, previous_counter_data.at(0).value);
+    ASSERT_GE(current_counter_data.at(1).value, previous_counter_data.at(1).value);
+    disableOutput();
+    previous_counter_data = current_counter_data;
   }
   {
     auto state_machine_data          = data.getStateMachineData();
@@ -185,15 +187,15 @@ TEST_F(FakeKeyenceTest, oneFaultyNonDecreasingData)
   }
   for (size_t i = 0; i < kNumIterations; ++i) {
     utils::concurrent::Thread::sleep(kSleepMillis);
-    for (size_t i = 0; i < fake_keyences.size(); ++i) {
-      const auto current_counter_data = fake_keyences.at(i).getData();
-      enableOutput();
-      if (current_counter_data.operational) {
-        ASSERT_GE(current_counter_data.value, previous_counter_data.at(i).value);
-      }
-      disableOutput();
-      previous_counter_data.at(i) = current_counter_data;
-    }
+    const std::array<data::CounterData, data::Sensors::kNumKeyence> current_counter_data
+      = {fake_keyences.at(0).getData(), fake_keyences.at(1).getData()};
+    enableOutput();
+    ASSERT_TRUE(current_counter_data.at(0).operational);
+    ASSERT_TRUE(current_counter_data.at(1).operational);
+    ASSERT_GE(current_counter_data.at(0).value, previous_counter_data.at(0).value);
+    ASSERT_GE(current_counter_data.at(1).value, previous_counter_data.at(1).value);
+    disableOutput();
+    previous_counter_data = current_counter_data;
   }
   {
     auto state_machine_data          = data.getStateMachineData();
@@ -202,15 +204,14 @@ TEST_F(FakeKeyenceTest, oneFaultyNonDecreasingData)
   }
   for (size_t i = 0; i < kNumIterations; ++i) {
     utils::concurrent::Thread::sleep(kSleepMillis);
-    for (size_t i = 0; i < fake_keyences.size(); ++i) {
-      const auto current_counter_data = fake_keyences.at(i).getData();
-      if (current_counter_data.operational) {
-        enableOutput();
-        ASSERT_GE(current_counter_data.value, previous_counter_data.at(i).value);
-        disableOutput();
-      }
-      previous_counter_data.at(i) = current_counter_data;
-    }
+    const std::array<data::CounterData, data::Sensors::kNumKeyence> current_counter_data
+      = {fake_keyences.at(0).getData(), fake_keyences.at(1).getData()};
+    enableOutput();
+    ASSERT_FALSE(current_counter_data.at(0).operational);
+    ASSERT_TRUE(current_counter_data.at(1).operational);
+    ASSERT_GE(current_counter_data.at(1).value, previous_counter_data.at(1).value);
+    disableOutput();
+    previous_counter_data = current_counter_data;
   }
   {
     auto state_machine_data          = data.getStateMachineData();
@@ -219,15 +220,119 @@ TEST_F(FakeKeyenceTest, oneFaultyNonDecreasingData)
   }
   for (size_t i = 0; i < kNumIterations; ++i) {
     utils::concurrent::Thread::sleep(kSleepMillis);
-    for (size_t i = 0; i < fake_keyences.size(); ++i) {
-      const auto current_counter_data = fake_keyences.at(i).getData();
-      enableOutput();
-      if (current_counter_data.operational) {
-        ASSERT_GE(current_counter_data.value, previous_counter_data.at(i).value);
-      }
-      disableOutput();
-      previous_counter_data.at(i) = current_counter_data;
-    }
+    const std::array<data::CounterData, data::Sensors::kNumKeyence> current_counter_data
+      = {fake_keyences.at(0).getData(), fake_keyences.at(1).getData()};
+    enableOutput();
+    ASSERT_FALSE(current_counter_data.at(0).operational);
+    ASSERT_TRUE(current_counter_data.at(1).operational);
+    ASSERT_GE(current_counter_data.at(1).value, previous_counter_data.at(1).value);
+    disableOutput();
+    previous_counter_data = current_counter_data;
+  }
+}
+
+//---------------------------------------------------------------------------
+// Two faulty
+//---------------------------------------------------------------------------
+
+TEST_F(FakeKeyenceTest, twoFaultyParsesConfig)
+{
+  auto fake_trajectory = std::make_shared<sensors::FakeTrajectory>(
+    *sensors::FakeTrajectory::fromFile(log_, kDefaultConfigPath));
+  enableOutput();
+  const auto fake_keyences_optional
+    = sensors::FakeKeyence::fromFile(log_, kTwoFaultyConfigPath, fake_trajectory);
+  ASSERT_TRUE(fake_keyences_optional);
+  const auto fake_keyences = *fake_keyences_optional;
+  ASSERT_EQ(data::Sensors::kNumKeyence, fake_keyences.size());
+  ASSERT_FLOAT_EQ(0.1, fake_keyences.at(0).getConfig().noise);
+  ASSERT_FLOAT_EQ(0.2, fake_keyences.at(1).getConfig().noise);
+  ASSERT_TRUE(fake_keyences.at(0).getConfig().failure_in_state);
+  ASSERT_TRUE(fake_keyences.at(1).getConfig().failure_in_state);
+  ASSERT_EQ(data::State::kAccelerating, *fake_keyences.at(0).getConfig().failure_in_state);
+  ASSERT_EQ(data::State::kNominalBraking, *fake_keyences.at(1).getConfig().failure_in_state);
+  disableOutput();
+}
+
+TEST_F(FakeKeyenceTest, twoFaultyNonDecreasingData)
+{
+  auto &data = data::Data::getInstance();
+  {
+    auto state_machine_data          = data.getStateMachineData();
+    state_machine_data.current_state = data::State::kIdle;
+    data.setStateMachineData(state_machine_data);
+  }
+  enableOutput();
+  const auto fake_trajectory = std::make_shared<sensors::FakeTrajectory>(
+    *sensors::FakeTrajectory::fromFile(log_, kDefaultConfigPath));
+  disableOutput();
+  auto fake_keyences_optional
+    = sensors::FakeKeyence::fromFile(log_, kTwoFaultyConfigPath, fake_trajectory);
+  ASSERT_TRUE(fake_keyences_optional);
+  auto fake_keyences = *fake_keyences_optional;
+  {
+    auto state_machine_data          = data.getStateMachineData();
+    state_machine_data.current_state = data::State::kAccelerating;
+    data.setStateMachineData(state_machine_data);
+  }
+  std::array<data::CounterData, fake_keyences.size()> previous_counter_data
+    = {fake_keyences.at(0).getData(), fake_keyences.at(1).getData()};
+  for (size_t i = 0; i < kNumIterations; ++i) {
+    utils::concurrent::Thread::sleep(kSleepMillis);
+    const std::array<data::CounterData, data::Sensors::kNumKeyence> current_counter_data
+      = {fake_keyences.at(0).getData(), fake_keyences.at(1).getData()};
+    enableOutput();
+    ASSERT_FALSE(current_counter_data.at(0).operational);
+    ASSERT_TRUE(current_counter_data.at(1).operational);
+    ASSERT_GE(current_counter_data.at(1).value, previous_counter_data.at(1).value);
+    disableOutput();
+    previous_counter_data = current_counter_data;
+  }
+  {
+    auto state_machine_data          = data.getStateMachineData();
+    state_machine_data.current_state = data::State::kCruising;
+    data.setStateMachineData(state_machine_data);
+  }
+  for (size_t i = 0; i < kNumIterations; ++i) {
+    utils::concurrent::Thread::sleep(kSleepMillis);
+    const std::array<data::CounterData, data::Sensors::kNumKeyence> current_counter_data
+      = {fake_keyences.at(0).getData(), fake_keyences.at(1).getData()};
+    enableOutput();
+    ASSERT_FALSE(current_counter_data.at(0).operational);
+    ASSERT_TRUE(current_counter_data.at(1).operational);
+    ASSERT_GE(current_counter_data.at(1).value, previous_counter_data.at(1).value);
+    disableOutput();
+    previous_counter_data = current_counter_data;
+  }
+  {
+    auto state_machine_data          = data.getStateMachineData();
+    state_machine_data.current_state = data::State::kNominalBraking;
+    data.setStateMachineData(state_machine_data);
+  }
+  for (size_t i = 0; i < kNumIterations; ++i) {
+    utils::concurrent::Thread::sleep(kSleepMillis);
+    const std::array<data::CounterData, data::Sensors::kNumKeyence> current_counter_data
+      = {fake_keyences.at(0).getData(), fake_keyences.at(1).getData()};
+    enableOutput();
+    ASSERT_FALSE(current_counter_data.at(0).operational);
+    ASSERT_FALSE(current_counter_data.at(1).operational);
+    disableOutput();
+    previous_counter_data = current_counter_data;
+  }
+  {
+    auto state_machine_data          = data.getStateMachineData();
+    state_machine_data.current_state = data::State::kEmergencyBraking;
+    data.setStateMachineData(state_machine_data);
+  }
+  for (size_t i = 0; i < kNumIterations; ++i) {
+    utils::concurrent::Thread::sleep(kSleepMillis);
+    const std::array<data::CounterData, data::Sensors::kNumKeyence> current_counter_data
+      = {fake_keyences.at(0).getData(), fake_keyences.at(1).getData()};
+    enableOutput();
+    ASSERT_FALSE(current_counter_data.at(0).operational);
+    ASSERT_FALSE(current_counter_data.at(1).operational);
+    disableOutput();
+    previous_counter_data = current_counter_data;
   }
 }
 

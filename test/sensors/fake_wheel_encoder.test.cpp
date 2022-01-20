f947f1b4b3a1496556746616462bcb70823db2e8
Franz Miltz <franz@miltz.me>
2022-01-20 07:59:32 +0000

    SNS: Four faulty fake wheel encoder tests

--- test/sensors/fake_wheel_encoder.test.cpp
+++ test/sensors/fake_wheel_encoder.test.cpp
@@ -55,17 +55,17 @@ TEST_F(FakeWheelEncoderTest, defaultNonDecreasingData)
   ASSERT_TRUE(fake_wheel_encoders_optional);
   disableOutput();
   auto fake_wheel_encoders = *fake_wheel_encoders_optional;
-  {
-    auto state_machine_data          = data.getStateMachineData();
-    state_machine_data.current_state = data::State::kAccelerating;
-    data.setStateMachineData(state_machine_data);
-  }
   std::array<data::CounterData, fake_wheel_encoders.size()> previous_counter_data = {
     fake_wheel_encoders.at(0).getData(),
     fake_wheel_encoders.at(1).getData(),
     fake_wheel_encoders.at(2).getData(),
     fake_wheel_encoders.at(3).getData(),
   };
+  {
+    auto state_machine_data          = data.getStateMachineData();
+    state_machine_data.current_state = data::State::kAccelerating;
+    data.setStateMachineData(state_machine_data);
+  }
   for (size_t i = 0; i < kNumIterations; ++i) {
     utils::concurrent::Thread::sleep(kSleepMillis);
     const std::array<data::CounterData, data::Sensors::kNumEncoders> current_counter_data = {
@@ -188,6 +188,7 @@ TEST_F(FakeWheelEncoderTest, oneFaultyParsesConfig)
   ASSERT_EQ(data::State::kNominalBraking, *fake_wheel_encoders.at(1).getConfig().failure_in_state);
   disableOutput();
 }
+
 TEST_F(FakeWheelEncoderTest, oneFaultyNonDecreasingData)
 {
   auto &data = data::Data::getInstance();
@@ -204,17 +205,17 @@ TEST_F(FakeWheelEncoderTest, oneFaultyNonDecreasingData)
   ASSERT_TRUE(fake_wheel_encoders_optional);
   disableOutput();
   auto fake_wheel_encoders = *fake_wheel_encoders_optional;
-  {
-    auto state_machine_data          = data.getStateMachineData();
-    state_machine_data.current_state = data::State::kAccelerating;
-    data.setStateMachineData(state_machine_data);
-  }
   std::array<data::CounterData, fake_wheel_encoders.size()> previous_counter_data = {
     fake_wheel_encoders.at(0).getData(),
     fake_wheel_encoders.at(1).getData(),
     fake_wheel_encoders.at(2).getData(),
     fake_wheel_encoders.at(3).getData(),
   };
+  {
+    auto state_machine_data          = data.getStateMachineData();
+    state_machine_data.current_state = data::State::kAccelerating;
+    data.setStateMachineData(state_machine_data);
+  }
   for (size_t i = 0; i < kNumIterations; ++i) {
     utils::concurrent::Thread::sleep(kSleepMillis);
     const std::array<data::CounterData, data::Sensors::kNumEncoders> current_counter_data = {
@@ -305,6 +306,150 @@ TEST_F(FakeWheelEncoderTest, oneFaultyNonDecreasingData)
     ASSERT_GE(current_counter_data.at(0).value, previous_counter_data.at(0).value);
     ASSERT_GE(current_counter_data.at(2).value, previous_counter_data.at(2).value);
     ASSERT_GE(current_counter_data.at(3).value, previous_counter_data.at(3).value);
+    disableOutput();
+    previous_counter_data = current_counter_data;
+  }
+}
+
+//---------------------------------------------------------------------------
+// Four faulty
+//---------------------------------------------------------------------------
+
+TEST_F(FakeWheelEncoderTest, fourFaultyParsesConfig)
+{
+  const auto fake_trajectory = std::make_shared<sensors::FakeTrajectory>(
+    *sensors::FakeTrajectory::fromFile(log_, kDefaultConfigPath));
+  const auto fake_wheel_encoders_optional
+    = sensors::FakeWheelEncoder::fromFile(log_, kFourFaultyConfigPath, fake_trajectory);
+  enableOutput();
+  ASSERT_TRUE(fake_wheel_encoders_optional);
+  const auto fake_wheel_encoders = *fake_wheel_encoders_optional;
+  ASSERT_EQ(data::Sensors::kNumEncoders, fake_wheel_encoders.size());
+  ASSERT_FLOAT_EQ(0.1, fake_wheel_encoders.at(0).getConfig().noise);
+  ASSERT_FLOAT_EQ(0.2, fake_wheel_encoders.at(1).getConfig().noise);
+  ASSERT_FLOAT_EQ(0.1, fake_wheel_encoders.at(2).getConfig().noise);
+  ASSERT_FLOAT_EQ(0.01, fake_wheel_encoders.at(3).getConfig().noise);
+  ASSERT_TRUE(fake_wheel_encoders.at(0).getConfig().failure_in_state);
+  ASSERT_TRUE(fake_wheel_encoders.at(1).getConfig().failure_in_state);
+  ASSERT_TRUE(fake_wheel_encoders.at(2).getConfig().failure_in_state);
+  ASSERT_TRUE(fake_wheel_encoders.at(3).getConfig().failure_in_state);
+  ASSERT_EQ(data::State::kEmergencyBraking,
+            *fake_wheel_encoders.at(0).getConfig().failure_in_state);
+  ASSERT_EQ(data::State::kAccelerating, *fake_wheel_encoders.at(1).getConfig().failure_in_state);
+  ASSERT_EQ(data::State::kCruising, *fake_wheel_encoders.at(2).getConfig().failure_in_state);
+  ASSERT_EQ(data::State::kNominalBraking, *fake_wheel_encoders.at(3).getConfig().failure_in_state);
+  disableOutput();
+}
+
+TEST_F(FakeWheelEncoderTest, fourFaultyNonDecreasingData)
+{
+  auto &data = data::Data::getInstance();
+  {
+    auto state_machine_data          = data.getStateMachineData();
+    state_machine_data.current_state = data::State::kIdle;
+    data.setStateMachineData(state_machine_data);
+  }
+  const auto fake_trajectory = std::make_shared<sensors::FakeTrajectory>(
+    *sensors::FakeTrajectory::fromFile(log_, kDefaultConfigPath));
+  auto fake_wheel_encoders_optional
+    = sensors::FakeWheelEncoder::fromFile(log_, kFourFaultyConfigPath, fake_trajectory);
+  enableOutput();
+  ASSERT_TRUE(fake_wheel_encoders_optional);
+  disableOutput();
+  auto fake_wheel_encoders = *fake_wheel_encoders_optional;
+  std::array<data::CounterData, fake_wheel_encoders.size()> previous_counter_data = {
+    fake_wheel_encoders.at(0).getData(),
+    fake_wheel_encoders.at(1).getData(),
+    fake_wheel_encoders.at(2).getData(),
+    fake_wheel_encoders.at(3).getData(),
+  };
+  {
+    auto state_machine_data          = data.getStateMachineData();
+    state_machine_data.current_state = data::State::kAccelerating;
+    data.setStateMachineData(state_machine_data);
+  }
+  for (size_t i = 0; i < kNumIterations; ++i) {
+    utils::concurrent::Thread::sleep(kSleepMillis);
+    const std::array<data::CounterData, data::Sensors::kNumEncoders> current_counter_data = {
+      fake_wheel_encoders.at(0).getData(),
+      fake_wheel_encoders.at(1).getData(),
+      fake_wheel_encoders.at(2).getData(),
+      fake_wheel_encoders.at(3).getData(),
+    };
+    enableOutput();
+    ASSERT_TRUE(current_counter_data.at(0).operational);
+    ASSERT_FALSE(current_counter_data.at(1).operational);
+    ASSERT_TRUE(current_counter_data.at(2).operational);
+    ASSERT_TRUE(current_counter_data.at(3).operational);
+    ASSERT_GE(current_counter_data.at(0).value, previous_counter_data.at(0).value);
+    ASSERT_GE(current_counter_data.at(2).value, previous_counter_data.at(2).value);
+    ASSERT_GE(current_counter_data.at(3).value, previous_counter_data.at(3).value);
+    disableOutput();
+    previous_counter_data = current_counter_data;
+  }
+  {
+    auto state_machine_data          = data.getStateMachineData();
+    state_machine_data.current_state = data::State::kCruising;
+    data.setStateMachineData(state_machine_data);
+  }
+  for (size_t i = 0; i < kNumIterations; ++i) {
+    utils::concurrent::Thread::sleep(kSleepMillis);
+    const std::array<data::CounterData, data::Sensors::kNumEncoders> current_counter_data = {
+      fake_wheel_encoders.at(0).getData(),
+      fake_wheel_encoders.at(1).getData(),
+      fake_wheel_encoders.at(2).getData(),
+      fake_wheel_encoders.at(3).getData(),
+    };
+    enableOutput();
+    ASSERT_TRUE(current_counter_data.at(0).operational);
+    ASSERT_FALSE(current_counter_data.at(1).operational);
+    ASSERT_FALSE(current_counter_data.at(2).operational);
+    ASSERT_TRUE(current_counter_data.at(3).operational);
+    ASSERT_GE(current_counter_data.at(0).value, previous_counter_data.at(0).value);
+    ASSERT_GE(current_counter_data.at(3).value, previous_counter_data.at(3).value);
+    disableOutput();
+    previous_counter_data = current_counter_data;
+  }
+  {
+    auto state_machine_data          = data.getStateMachineData();
+    state_machine_data.current_state = data::State::kNominalBraking;
+    data.setStateMachineData(state_machine_data);
+  }
+  for (size_t i = 0; i < kNumIterations; ++i) {
+    utils::concurrent::Thread::sleep(kSleepMillis);
+    const std::array<data::CounterData, data::Sensors::kNumEncoders> current_counter_data = {
+      fake_wheel_encoders.at(0).getData(),
+      fake_wheel_encoders.at(1).getData(),
+      fake_wheel_encoders.at(2).getData(),
+      fake_wheel_encoders.at(3).getData(),
+    };
+    enableOutput();
+    ASSERT_TRUE(current_counter_data.at(0).operational);
+    ASSERT_FALSE(current_counter_data.at(1).operational);
+    ASSERT_FALSE(current_counter_data.at(2).operational);
+    ASSERT_FALSE(current_counter_data.at(3).operational);
+    ASSERT_GE(current_counter_data.at(0).value, previous_counter_data.at(0).value);
+    disableOutput();
+    previous_counter_data = current_counter_data;
+  }
+  {
+    auto state_machine_data          = data.getStateMachineData();
+    state_machine_data.current_state = data::State::kEmergencyBraking;
+    data.setStateMachineData(state_machine_data);
+  }
+  for (size_t i = 0; i < kNumIterations; ++i) {
+    utils::concurrent::Thread::sleep(kSleepMillis);
+    const std::array<data::CounterData, data::Sensors::kNumEncoders> current_counter_data = {
+      fake_wheel_encoders.at(0).getData(),
+      fake_wheel_encoders.at(1).getData(),
+      fake_wheel_encoders.at(2).getData(),
+      fake_wheel_encoders.at(3).getData(),
+    };
+    enableOutput();
+    ASSERT_FALSE(current_counter_data.at(0).operational);
+    ASSERT_FALSE(current_counter_data.at(1).operational);
+    ASSERT_FALSE(current_counter_data.at(2).operational);
+    ASSERT_FALSE(current_counter_data.at(3).operational);
     disableOutput();
     previous_counter_data = current_counter_data;
   }

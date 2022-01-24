dea70860d6b42c1fbaf5478140cbaf80b5df4eff
rachel275 <rachsomerset>
2022-01-24 18:56:33 +0000

    re-push data.c(h)pp after configuring code

--- src/data/data.cpp
+++ src/data/data.cpp
@@ -1,179 +1,168 @@
 #include "data.hpp"
 
-namespace hyped
+namespace hyped {
+
+// imports
+using utils::concurrent::ScopedLock;
+
+namespace data {
+
+const char *states[num_states] = {
+  "Idle",           "PreCalibrating",   "Calibrating",    "Ready",    "Accelerating", "Cruising",
+  "NominalBraking", "EmergencyBraking", "FailureStopped", "Finished", "Invalid",
+};
+
+Data &Data::getInstance()
 {
+  static Data d;
+  return d;
+}
 
-  // imports
-  using utils::concurrent::ScopedLock;
+StateMachine Data::getStateMachineData()
+{
+  ScopedLock L(&lock_state_machine_);
+  return state_machine_;
+}
 
-  namespace data
-  {
+void Data::setStateMachineData(const StateMachine &sm_data)
+{
+  ScopedLock L(&lock_state_machine_);
+  state_machine_ = sm_data;
+}
 
-    const char *states[num_states] = {
-        "Idle",
-        "PreCalibrating",
-        "Calibrating",
-        "Ready",
-        "Accelerating",
-        "Cruising",
-        "NominalBraking",
-        "EmergencyBraking",
-        "FailureStopped",
-        "Finished",
-        "Invalid",
-    };
+Navigation Data::getNavigationData()
+{
+  ScopedLock L(&lock_navigation_);
+  return navigation_;
+}
 
-    Data &Data::getInstance()
-    {
-      static Data d;
-      return d;
-    }
+void Data::setNavigationData(const Navigation &nav_data)
+{
+  ScopedLock L(&lock_navigation_);
+  navigation_ = nav_data;
+}
 
-    StateMachine Data::getStateMachineData()
-    {
-      ScopedLock L(&lock_state_machine_);
-      return state_machine_;
-    }
+Sensors Data::getSensorsData()
+{
+  ScopedLock L(&lock_sensors_);
+  return sensors_;
+}
 
-    void Data::setStateMachineData(const StateMachine &sm_data)
-    {
-      ScopedLock L(&lock_state_machine_);
-      state_machine_ = sm_data;
-    }
+DataPoint<array<ImuData, Sensors::kNumImus>> Data::getSensorsImuData()
+{
+  ScopedLock L(&lock_sensors_);
+  return sensors_.imu;
+}
 
-    Navigation Data::getNavigationData()
-    {
-      ScopedLock L(&lock_navigation_);
-      return navigation_;
-    }
+DataPoint<array<EncoderData, Sensors::kNumEncoders>> Data::getSensorsEncoderData()
+{
+  ScopedLock L(&lock_sensors_);
+  return sensors_.encoder;
+}
 
-    void Data::setNavigationData(const Navigation &nav_data)
-    {
-      ScopedLock L(&lock_navigation_);
-      navigation_ = nav_data;
-    }
+array<StripeCounter, Sensors::kNumKeyence> Data::getSensorsKeyenceData()
+{
+  ScopedLock L(&lock_sensors_);
+  return sensors_.keyence_stripe_counter;
+}
 
-    Sensors Data::getSensorsData()
-    {
-      ScopedLock L(&lock_sensors_);
-      return sensors_;
-    }
+int Data::getTemperature()
+{
+  ScopedLock L(&lock_temp_);
+  return temperature_;
+}
 
-    DataPoint<array<ImuData, Sensors::kNumImus>> Data::getSensorsImuData()
-    {
-      ScopedLock L(&lock_sensors_);
-      return sensors_.imu;
-    }
+void Data::setTemperature(const int &temp)
+{
+  ScopedLock L(&lock_temp_);
+  temperature_ = temp;
+}
 
-    DataPoint<array<EncoderData, Sensors::kNumEncoders>> Data::getSensorsEncoderData()
-    {
-      ScopedLock L(&lock_sensors_);
-      return sensors_.encoder;
-    }
+int Data::getPressure()
+{
+  ScopedLock L(&lock_pressure_);
+  return pressure_;
+}
 
-    array<StripeCounter, Sensors::kNumKeyence> Data::getSensorsKeyenceData()
-    {
-      ScopedLock L(&lock_sensors_);
-      return sensors_.keyence_stripe_counter;
-    }
+void Data::setPressure(const int &pressure)
+{
+  ScopedLock L(&lock_pressure_);
+  pressure_ = pressure;
+}
 
-    int Data::getTemperature()
-    {
-      ScopedLock L(&lock_temp_);
-      return temperature_;
-    }
+void Data::setSensorsData(const Sensors &sensors_data)
+{
+  ScopedLock L(&lock_sensors_);
+  sensors_ = sensors_data;
+}
 
-    void Data::setTemperature(const int &temp)
-    {
-      ScopedLock L(&lock_temp_);
-      temperature_ = temp;
-    }
+void Data::setSensorsImuData(const DataPoint<array<ImuData, Sensors::kNumImus>> &imu)
+{
+  ScopedLock L(&lock_sensors_);
+  sensors_.imu = imu;
+}
 
-    int Data::getPressure()
-    {
-      ScopedLock L(&lock_pressure_);
-      return pressure_;
-    }
+void Data::setSensorsEncoderData(
+  const DataPoint<array<EncoderData, Sensors::kNumEncoders>> &encoder)  // NOLINT
+{
+  ScopedLock L(&lock_sensors_);
+  sensors_.encoder = encoder;
+}
 
-    void Data::setPressure(const int &pressure)
-    {
-      ScopedLock L(&lock_pressure_);
-      pressure_ = pressure;
-    }
+void Data::setSensorsKeyenceData(
+  const array<StripeCounter, Sensors::kNumKeyence> &keyence_stripe_counter)  // NOLINT
+{
+  ScopedLock L(&lock_sensors_);
+  sensors_.keyence_stripe_counter = keyence_stripe_counter;
+}
 
-    void Data::setSensorsData(const Sensors &sensors_data)
-    {
-      ScopedLock L(&lock_sensors_);
-      sensors_ = sensors_data;
-    }
+Batteries Data::getBatteriesData()
+{
+  ScopedLock L(&lock_batteries_);
+  return batteries_;
+}
 
-    void Data::setSensorsImuData(const DataPoint<array<ImuData, Sensors::kNumImus>> &imu)
-    {
-      ScopedLock L(&lock_sensors_);
-      sensors_.imu = imu;
-    }
+void Data::setBatteriesData(const Batteries &batteries_data)
+{
+  ScopedLock L(&lock_batteries_);
+  batteries_ = batteries_data;
+}
 
-    void Data::setSensorsEncoderData(
-        const DataPoint<array<EncoderData, Sensors::kNumEncoders>> &encoder) // NOLINT
-    {
-      ScopedLock L(&lock_sensors_);
-      sensors_.encoder = encoder;
-    }
+EmergencyBrakes Data::getEmergencyBrakesData()
+{
+  ScopedLock L(&lock_emergency_brakes_);
+  return emergency_brakes_;
+}
 
-    void Data::setSensorsKeyenceData(
-        const array<StripeCounter, Sensors::kNumKeyence> &keyence_stripe_counter) // NOLINT
-    {
-      ScopedLock L(&lock_sensors_);
-      sensors_.keyence_stripe_counter = keyence_stripe_counter;
-    }
+void Data::setEmergencyBrakesData(const EmergencyBrakes &emergency_brakes_data)
+{
+  ScopedLock L(&lock_emergency_brakes_);
+  emergency_brakes_ = emergency_brakes_data;
+}
 
-    Batteries Data::getBatteriesData()
-    {
-      ScopedLock L(&lock_batteries_);
-      return batteries_;
-    }
+Motors Data::getMotorData()
+{
+  ScopedLock L(&lock_motors_);
+  return motors_;
+}
 
-    void Data::setBatteriesData(const Batteries &batteries_data)
-    {
-      ScopedLock L(&lock_batteries_);
-      batteries_ = batteries_data;
-    }
+void Data::setMotorData(const Motors &motor_data)
+{
+  ScopedLock L(&lock_motors_);
+  motors_ = motor_data;
+}
 
-    EmergencyBrakes Data::getEmergencyBrakesData()
-    {
-      ScopedLock L(&lock_emergency_brakes_);
-      return emergency_brakes_;
-    }
+Telemetry Data::getTelemetryData()
+{
+  ScopedLock L(&lock_telemetry_);
+  return telemetry_;
+}
 
-    void Data::setEmergencyBrakesData(const EmergencyBrakes &emergency_brakes_data)
-    {
-      ScopedLock L(&lock_emergency_brakes_);
-      emergency_brakes_ = emergency_brakes_data;
-    }
+void Data::setTelemetryData(const Telemetry &telemetry_data)
+{
+  ScopedLock L(&lock_telemetry_);
+  telemetry_ = telemetry_data;
+}
 
-    Motors Data::getMotorData()
-    {
-      ScopedLock L(&lock_motors_);
-      return motors_;
-    }
-
-    void Data::setMotorData(const Motors &motor_data)
-    {
-      ScopedLock L(&lock_motors_);
-      motors_ = motor_data;
-    }
-
-    Telemetry Data::getTelemetryData()
-    {
-      ScopedLock L(&lock_telemetry_);
-      return telemetry_;
-    }
-
-    void Data::setTelemetryData(const Telemetry &telemetry_data)
-    {
-      ScopedLock L(&lock_telemetry_);
-      telemetry_ = telemetry_data;
-    }
-
-  } // namespace data
-} // namespace hyped
+}  // namespace data
+}  // namespace hyped
--- src/data/data.hpp
+++ src/data/data.hpp
@@ -12,351 +12,331 @@
 using std::array;
 using std::vector;
 
-namespace hyped
-{
+namespace hyped {
 
-  // imports
-  using utils::concurrent::Lock;
-  using utils::math::Vector;
+// imports
+using utils::concurrent::Lock;
+using utils::math::Vector;
 
-  namespace data
-  {
+namespace data {
 
-    // -------------------------------------------------------------------------------------------------
-    // Global Module States
-    // -------------------------------------------------------------------------------------------------
-    enum class ModuleStatus
-    {
-      kCriticalFailure, // state machine transitions to EmergencyBraking/FailureStopped
-      kStart,           // Initial module state
-      kInit,            // state machine transistions to Calibrating if all modules have Init or Ready status.
-      kReady,           // state machine transistions to Ready if all modules have Ready status.
-    };
+// -------------------------------------------------------------------------------------------------
+// Global Module States
+// -------------------------------------------------------------------------------------------------
+enum class ModuleStatus {
+  kCriticalFailure,  // state machine transitions to EmergencyBraking/FailureStopped
+  kStart,            // Initial module state
+  kInit,   // state machine transistions to Calibrating if all modules have Init or Ready status.
+  kReady,  // state machine transistions to Ready if all modules have Ready status.
+};
 
-    struct Module
-    {
-      ModuleStatus module_status = ModuleStatus::kStart;
-    };
+struct Module {
+  ModuleStatus module_status = ModuleStatus::kStart;
+};
 
-    // -------------------------------------------------------------------------------------------------
-    // Navigation
-    // -------------------------------------------------------------------------------------------------
-    typedef float nav_t;
-    typedef Vector<nav_t, 3> NavigationVector;
-    struct Navigation : public Module
-    {
-      static constexpr nav_t kMaximumVelocity = 100; // m/s
-      static constexpr nav_t kRunLength = 1250.0;    // m
-      static constexpr nav_t kBrakingBuffer = 20.0;  // m
-      nav_t displacement;                            // m
-      nav_t velocity;                                // m/s
-      nav_t acceleration;                            // m/s^2
-      nav_t emergency_braking_distance;              // m
-      nav_t braking_distance = 750;                  // m
-    };
+// -------------------------------------------------------------------------------------------------
+// Navigation
+// -------------------------------------------------------------------------------------------------
+typedef float nav_t;
+typedef Vector<nav_t, 3> NavigationVector;
+struct Navigation : public Module {
+  static constexpr nav_t kMaximumVelocity = 100;     // m/s
+  static constexpr nav_t kRunLength       = 1250.0;  // m
+  static constexpr nav_t kBrakingBuffer   = 20.0;    // m
+  nav_t displacement;                                // m
+  nav_t velocity;                                    // m/s
+  nav_t acceleration;                                // m/s^2
+  nav_t emergency_braking_distance;                  // m
+  nav_t braking_distance = 750;                      // m
+};
 
-    // -------------------------------------------------------------------------------------------------
-    // Raw Sensor data
-    // -------------------------------------------------------------------------------------------------
-    struct Sensor
-    {
-      bool operational;
-    };
+// -------------------------------------------------------------------------------------------------
+// Raw Sensor data
+// -------------------------------------------------------------------------------------------------
+struct Sensor {
+  bool operational;
+};
 
-    struct ImuData : public Sensor
-    {
-      NavigationVector acc;
+struct ImuData : public Sensor {
+  NavigationVector acc;
 
-      std::vector<NavigationVector> fifo;
-    };
+  std::vector<NavigationVector> fifo;
+};
 
-    struct EncoderData : public Sensor
-    {
-      nav_t disp;
-    };
+struct EncoderData : public Sensor {
+  nav_t disp;
+};
 
-    struct StripeCounter : public Sensor
-    {
-      DataPoint<uint32_t> count;
-    };
+struct StripeCounter : public Sensor {
+  DataPoint<uint32_t> count;
+};
 
-    struct TemperatureData : public Sensor
-    {
-      int temp; // C
-    };
+struct TemperatureData : public Sensor {
+  int temp;  // C
+};
 
-    struct PressureData : public Sensor
-    {
-      int pressure; //mbar
-    };
+struct PressureData : public Sensor {
+  int pressure;  // mbar
+};
 
-    struct Sensors : public Module
-    {
-      static constexpr int kNumImus = 4;
-      static constexpr int kNumEncoders = 4;
-      static constexpr int kNumKeyence = 2;
+struct Sensors : public Module {
+  static constexpr int kNumImus     = 4;
+  static constexpr int kNumEncoders = 4;
+  static constexpr int kNumKeyence  = 2;
 
-      DataPoint<array<ImuData, kNumImus>> imu;
-      DataPoint<array<EncoderData, kNumEncoders>> encoder;
-      array<StripeCounter, kNumKeyence> keyence_stripe_counter;
-    };
+  DataPoint<array<ImuData, kNumImus>> imu;
+  DataPoint<array<EncoderData, kNumEncoders>> encoder;
+  array<StripeCounter, kNumKeyence> keyence_stripe_counter;
+};
 
-    struct BatteryData
-    {
-      static constexpr int kNumCells = 36;
-      uint16_t voltage;           // dV
-      int16_t current;            // dA
-      uint8_t charge;             // %
-      int8_t average_temperature; // C
+struct BatteryData {
+  static constexpr int kNumCells = 36;
+  uint16_t voltage;            // dV
+  int16_t current;             // dA
+  uint8_t charge;              // %
+  int8_t average_temperature;  // C
 
-      // below only for BMSHP! Value for BMSLP = 0
-      uint16_t cell_voltage[kNumCells]; // mV
-      int8_t low_temperature;           // C
-      int8_t high_temperature;          // C
-      uint16_t low_voltage_cell;        // mV
-      uint16_t high_voltage_cell;       // mV
-      bool imd_fault;
-    };
+  // below only for BMSHP! Value for BMSLP = 0
+  uint16_t cell_voltage[kNumCells];  // mV
+  int8_t low_temperature;            // C
+  int8_t high_temperature;           // C
+  uint16_t low_voltage_cell;         // mV
+  uint16_t high_voltage_cell;        // mV
+  bool imd_fault;
+};
 
-    struct Batteries : public Module
-    {
-      static constexpr int kNumLPBatteries = 2;
-      static constexpr int kNumHPBatteries = 1;
+struct Batteries : public Module {
+  static constexpr int kNumLPBatteries = 2;
+  static constexpr int kNumHPBatteries = 1;
 
-      array<BatteryData, kNumLPBatteries> low_power_batteries;
-      array<BatteryData, kNumHPBatteries> high_power_batteries;
-    };
+  array<BatteryData, kNumLPBatteries> low_power_batteries;
+  array<BatteryData, kNumHPBatteries> high_power_batteries;
+};
 
-    struct EmergencyBrakes : public Module
-    {
-      static constexpr int kBrakeCommandWaitTime = 1000; // milliseconds
-      static constexpr int kNumBrakes = 2;
-      bool brakes_retracted[kNumBrakes] = {false}; // true if brakes retract
-    };
+struct EmergencyBrakes : public Module {
+  static constexpr int kBrakeCommandWaitTime = 1000;  // milliseconds
+  static constexpr int kNumBrakes            = 2;
+  bool brakes_retracted[kNumBrakes]          = {false};  // true if brakes retract
+};
 
-    // -------------------------------------------------------------------------------------------------
-    // Motor data
-    // -------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------
+// Motor data
+// -------------------------------------------------------------------------------------------------
 
-    struct Motors : public Module
-    {
-      static constexpr int kNumMotors = 4;
-      std::array<uint32_t, kNumMotors> rpms = {{0, 0, 0, 0}};
-    };
+struct Motors : public Module {
+  static constexpr int kNumMotors       = 4;
+  std::array<uint32_t, kNumMotors> rpms = {{0, 0, 0, 0}};
+};
 
-    // -------------------------------------------------------------------------------------------------
-    // Telemetry data
-    // -------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------
+// Telemetry data
+// -------------------------------------------------------------------------------------------------
 
-    struct Telemetry : public Module
-    {
-      bool calibrate_command = false;
-      bool launch_command = false;
-      bool shutdown_command = false;
-      bool service_propulsion_go = false;
-      bool emergency_stop_command = false;
-      bool nominal_braking_command = true;
-    };
+struct Telemetry : public Module {
+  bool calibrate_command       = false;
+  bool launch_command          = false;
+  bool shutdown_command        = false;
+  bool service_propulsion_go   = false;
+  bool emergency_stop_command  = false;
+  bool nominal_braking_command = true;
+};
 
-    // -------------------------------------------------------------------------------------------------
-    // State Machine States
-    // -------------------------------------------------------------------------------------------------
-    enum State
-    {
-      kIdle,
-      kPreCalibrating,
-      kCalibrating,
-      kReady,
-      kAccelerating,
-      kCruising,
-      kNominalBraking,
-      kEmergencyBraking,
-      kFailureStopped,
-      kFinished,
-      kInvalid,
-      num_states
-    };
+// -------------------------------------------------------------------------------------------------
+// State Machine States
+// -------------------------------------------------------------------------------------------------
+enum State {
+  kIdle,
+  kPreCalibrating,
+  kCalibrating,
+  kReady,
+  kAccelerating,
+  kCruising,
+  kNominalBraking,
+  kEmergencyBraking,
+  kFailureStopped,
+  kFinished,
+  kInvalid,
+  num_states
+};
 
-    extern const char *states[num_states];
+extern const char *states[num_states];
 
-    struct StateMachine
-    {
-      bool critical_failure;
-      State current_state;
-    };
+struct StateMachine {
+  bool critical_failure;
+  State current_state;
+};
 
-    // -------------------------------------------------------------------------------------------------
-    // Common Data structure/class
-    // -------------------------------------------------------------------------------------------------
-    /**
+// -------------------------------------------------------------------------------------------------
+// Common Data structure/class
+// -------------------------------------------------------------------------------------------------
+/**
  * @brief      A singleton class managing the data exchange between sub-team
  * threads.
  */
-    class Data
-    {
-    public:
-      /**
+class Data {
+ public:
+  /**
    * @brief      Always returns a reference to the only instance of `Data`.
    */
-      static Data &getInstance();
+  static Data &getInstance();
 
-      /**
+  /**
    * @brief      Retrieves data related to the state machine. Data has high priority.
    */
-      StateMachine getStateMachineData();
+  StateMachine getStateMachineData();
 
-      /**
+  /**
    * @brief      Should be called by state machine team to update data.
    */
-      void setStateMachineData(const StateMachine &sm_data);
+  void setStateMachineData(const StateMachine &sm_data);
 
-      /**
+  /**
    * @brief      Retrieves data produced by navigation sub-team.
    */
-      Navigation getNavigationData();
+  Navigation getNavigationData();
 
-      /**
+  /**
    * @brief      Should be called by navigation sub-team whenever they have new data.
    */
-      void setNavigationData(const Navigation &nav_data);
+  void setNavigationData(const Navigation &nav_data);
 
-      /**
+  /**
    * @brief Get the Temperature from averaged thermistor values
    *
    * @return int temperature in degrees C
    */
-      int getTemperature();
+  int getTemperature();
 
-      /**
+  /**
    * @brief Set the Temperature from averaged thermistor values
    *
    * @param temp - temp in degrees C
    */
-      void setTemperature(const int &temp);
+  void setTemperature(const int &temp);
 
-      /**
+  /**
    * @brief Get the Pressure
    *
    * @return int pressure in bar
    */
-      int getPressure();
+  int getPressure();
 
-      /**
+  /**
    * @brief Set the Pressure
    *
    * @param pressure - pressure in bar
    */
-      void setPressure(const int &pressure);
+  void setPressure(const int &pressure);
 
-      /**
+  /**
    * @brief      Retrieves data from all sensors
    */
-      Sensors getSensorsData();
+  Sensors getSensorsData();
 
-      /**
+  /**
    * @brief retrieves imu data from Sensors
    */
-      DataPoint<array<ImuData, Sensors::kNumImus>> getSensorsImuData();
+  DataPoint<array<ImuData, Sensors::kNumImus>> getSensorsImuData();
 
-      /**
+  /**
    * @brief retrieves encoder data from Sensors
    */
-      DataPoint<array<EncoderData, Sensors::kNumEncoders>> getSensorsEncoderData();
+  DataPoint<array<EncoderData, Sensors::kNumEncoders>> getSensorsEncoderData();
 
-      /**
+  /**
    * @brief retrieves gpio_counter data from Sensors
    */
-      array<StripeCounter, Sensors::kNumKeyence> getSensorsKeyenceData();
+  array<StripeCounter, Sensors::kNumKeyence> getSensorsKeyenceData();
 
-      /**
+  /**
    * @brief      Should be called to update sensor data.
    */
-      void setSensorsData(const Sensors &sensors_data);
-      /**
+  void setSensorsData(const Sensors &sensors_data);
+  /**
    * @brief      Should be called to update sensor imu data.
    */
-      void setSensorsImuData(const DataPoint<array<ImuData, Sensors::kNumImus>> &imu);
-      /**
+  void setSensorsImuData(const DataPoint<array<ImuData, Sensors::kNumImus>> &imu);
+  /**
    * @brief      Should be called to update sensor encoder data.
    */
-      void setSensorsEncoderData(const DataPoint<array<EncoderData, Sensors::kNumEncoders>> &imu);
-      /**
+  void setSensorsEncoderData(const DataPoint<array<EncoderData, Sensors::kNumEncoders>> &imu);
+  /**
    * @brief      Should be called to update sensor keyence data.
    */
-      void setSensorsKeyenceData(
-          const array<StripeCounter, Sensors::kNumKeyence> &keyence_stripe_counter); // NOLINT
+  void setSensorsKeyenceData(
+    const array<StripeCounter, Sensors::kNumKeyence> &keyence_stripe_counter);  // NOLINT
 
-      /**
+  /**
    * @brief      Retrieves data from the batteries.
    */
-      Batteries getBatteriesData();
+  Batteries getBatteriesData();
 
-      /**
+  /**
    * @brief      Should be called to update battery data
    */
-      void setBatteriesData(const Batteries &batteries_data);
+  void setBatteriesData(const Batteries &batteries_data);
 
-      /**
+  /**
    * @brief      Retrieves data from the emergency brakes.
    */
-      EmergencyBrakes getEmergencyBrakesData();
+  EmergencyBrakes getEmergencyBrakesData();
 
-      /**
+  /**
    * @brief      Should be called to update emergency brakes data
    */
-      void setEmergencyBrakesData(const EmergencyBrakes &emergency_brakes_data);
+  void setEmergencyBrakesData(const EmergencyBrakes &emergency_brakes_data);
 
-      /**
+  /**
    * @brief      Retrieves data produced by each of the four motors.
    */
-      Motors getMotorData();
+  Motors getMotorData();
 
-      /**
+  /**
    * @brief      Should be called to update motor data.
    */
-      void setMotorData(const Motors &motor_data);
+  void setMotorData(const Motors &motor_data);
 
-      /**
+  /**
    * @brief      Retrieves data on whether stop/kill power commands have been issued.
    */
-      Telemetry getTelemetryData();
+  Telemetry getTelemetryData();
 
-      /**
+  /**
    * @brief      Should be called to update communications data.
    */
-      void setTelemetryData(const Telemetry &telemetry_data);
+  void setTelemetryData(const Telemetry &telemetry_data);
 
-    private:
-      StateMachine state_machine_;
-      Navigation navigation_;
-      Sensors sensors_;
-      Motors motors_;
-      Batteries batteries_;
-      Telemetry telemetry_;
-      EmergencyBrakes emergency_brakes_;
-      int temperature_; // In degrees C
-      int pressure_;
+ private:
+  StateMachine state_machine_;
+  Navigation navigation_;
+  Sensors sensors_;
+  Motors motors_;
+  Batteries batteries_;
+  Telemetry telemetry_;
+  EmergencyBrakes emergency_brakes_;
+  int temperature_;  // In degrees C
+  int pressure_;
 
-      // locks for data substructures
-      Lock lock_state_machine_;
-      Lock lock_navigation_;
-      Lock lock_sensors_;
-      Lock lock_motors_;
-      Lock lock_temp_;
-      Lock lock_pressure_;
+  // locks for data substructures
+  Lock lock_state_machine_;
+  Lock lock_navigation_;
+  Lock lock_sensors_;
+  Lock lock_motors_;
+  Lock lock_temp_;
+  Lock lock_pressure_;
 
-      Lock lock_telemetry_;
-      Lock lock_batteries_;
-      Lock lock_emergency_brakes_;
+  Lock lock_telemetry_;
+  Lock lock_batteries_;
+  Lock lock_emergency_brakes_;
 
-      Data() {}
+  Data() {}
 
-    public:
-      Data(const Data &) = delete;
-      Data &operator=(const Data &) = delete;
-      Data(Data &&) = delete;
-      Data &operator=(Data &&) = delete;
-    };
+ public:
+  Data(const Data &) = delete;
+  Data &operator=(const Data &) = delete;
+  Data(Data &&)                 = delete;
+  Data &operator=(Data &&) = delete;
+};
 
-  } // namespace data
-} // namespace hyped
+}  // namespace data
+}  // namespace hyped

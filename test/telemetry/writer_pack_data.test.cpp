abf46242769afeeb27fd67c06143d88b36d696dd
dhb03 <hb.Dorna@yahoo.co.uk>
2022-02-06 17:31:20 +0000

    Adding test JSON strings

--- test/telemetry/writer_pack_data.test.cpp
+++ test/telemetry/writer_pack_data.test.cpp
@@ -1,5 +1,3 @@
-#include <string>
-
 #include "data/data.hpp"
 #include "gtest/gtest.h"
 #include "telemetry/writer.hpp"
@@ -26,7 +24,7 @@ TEST_F(WriterPackData, packsTelemetryData)
   writer.packTelemetryData();
   writer.end();
   const std::string actualJson   = writer.getString();
-  const std::string expectedJson = "INSERT JSON HERE";  // TODO
+  const std::string expectedJson = {"calibrate": bool,"emergency_stop": bool, "launch": bool , "nominal_breaking": bool ,"service_propulsion_go":bool,"shutdown":bool,"telemetry_status":String};  // TODO
   ASSERT_EQ(actualJson, expectedJson) << "Telemetry json does not match expected output.";
 }
 
@@ -38,7 +36,7 @@ TEST_F(WriterPackData, packsNavigationData)
   writer.packNavigationData();
   writer.end();
   const std::string actualJson   = writer.getString();
-  const std::string expectedJson = "INSERT JSON HERE";  // TODO
+  const std::string expectedJson = {"braking_distance": 750, "displacement":1250,"emergency_braking_distance":20,"velocity":100,"acceleration":8};  // TODO
   ASSERT_EQ(actualJson, expectedJson) << "Navigation json does not match expected output.";
 }
 
@@ -50,7 +48,7 @@ TEST_F(WriterPackData, packsSensorsData)
   writer.packSensorsData();
   writer.end();
   const std::string actualJson   = writer.getString();
-  const std::string expectedJson = "INSERT JSON HERE";  // TODO
+  const std::string expectedJson = {[{"lp_battery":{"average_temp":int, "voltage":int,"current":int,"charge":int,"low_temp":int,"high_temp":int,"low_voltage_cell":int,"high_voltage_cell":int,"imd_fault": false}},{"lp_battery":{"average_temp":int, "voltage":int,"current":int,"charge":int,"low_temp":int,"high_temp":int,"low_voltage_cell":int,"high_voltage_cell":int,"imd_fault": false}}],[{"hp_battery":{"average_temp":int, "voltage":int,"current":int,"charge":int,"low_temp":int,"high_temp":int,"low_voltage_cell":int,"high_voltage_cell":int,"imd_fault": false}}], "brakes_retracted": bool,"temperature":int,"brake status": String,"sensors_status":bool, "batteries_status": bool};  // TODO
   ASSERT_EQ(actualJson, expectedJson) << "Sensors json does not match expected output.";
 }
 
@@ -62,7 +60,7 @@ TEST_F(WriterPackData, packsMotorData)
   writer.packMotorData();
   writer.end();
   const std::string actualJson   = writer.getString();
-  const std::string expectedJson = "INSERT JSON HERE";  // TODO
+  const std::string expectedJson = {"motor_rpms":[0,0,0,0],"motors_status":String} ;  // TODO
   ASSERT_EQ(actualJson, expectedJson) << "Motor json does not match expected output.";
 }
 
@@ -74,7 +72,7 @@ TEST_F(WriterPackData, packsStateMachineData)
   writer.packStateMachineData();
   writer.end();
   const std::string actualJson   = writer.getString();
-  const std::string expectedJson = "INSERT JSON HERE";  // TODO
+  const std::string expectedJson = {"critical_failure": bool,"current_state": String};  // TODO
   ASSERT_EQ(actualJson, expectedJson) << "State machine json does not match expected output.";
 }
 
@@ -86,6 +84,6 @@ TEST_F(WriterPackData, packsBatteryData)
   // writer.packBattery();
   writer.end();
   const std::string actualJson   = writer.getString();
-  const std::string expectedJson = "INSERT JSON HERE";  // TODO
+  const std::string expectedJson =  <value>: {"average_temp":int, "voltage":int,"current":int,"charge":int,"low_temp":int,"high_temp":int,"low_voltage_cell":int,"high_voltage_cell":int,"imd_fault": bool};  // TODO
   ASSERT_EQ(actualJson, expectedJson) << "Battery json does not match expected output.";
 }

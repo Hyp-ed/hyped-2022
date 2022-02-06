d592586806485b1b553683355a3e784be134a242
dhb03 <hb.Dorna@yahoo.co.uk>
2022-02-06 17:51:50 +0000

    Adding JSON strings

--- test/telemetry/writer_pack_data.test.cpp
+++ test/telemetry/writer_pack_data.test.cpp
@@ -110,7 +110,7 @@ TEST_F(WriterPackData, packsId)
   writer.packId(5);
   writer.end();
   const std::string actualJson   = writer.getString();
-  const std::string expectedJson = "INSERT JSON HERE";  // TODO
+  const std::string expectedJson = {"id": int};  // TODO
   ASSERT_EQ(actualJson, expectedJson) << "ID json does not match expected output.";
   ASSERT_TRUE(writer.isValidJson()) << "ID json invalid.";
 }
@@ -123,7 +123,7 @@ TEST_F(WriterPackData, packsTelemetryData)
   writer.packTelemetryData();
   writer.end();
   const std::string actualJson   = writer.getString();
-  const std::string expectedJson = {"calibrate": bool,"emergency_stop": bool, "launch": bool , "nominal_breaking": bool ,"service_propulsion_go":bool,"shutdown":bool,"telemetry_status":String};  // TODO
+  const std::string expectedJson = {"telemetry": {"calibrate": bool,"emergency_stop": bool, "launch": bool , "nominal_breaking": bool ,"service_propulsion_go":bool,"shutdown":bool,"telemetry_status":String}};  // TODO
   ASSERT_EQ(actualJson, expectedJson) << "Telemetry json does not match expected output.";
   ASSERT_TRUE(writer.isValidJson()) << "Telemetry json invalid.";
 }
@@ -136,7 +136,7 @@ TEST_F(WriterPackData, packsNavigationData)
   writer.packNavigationData();
   writer.end();
   const std::string actualJson   = writer.getString();
-  const std::string expectedJson = {"braking_distance": 750, "displacement":1250,"emergency_braking_distance":20,"velocity":100,"acceleration":8};  // TODO
+  const std::string expectedJson = {"navigation": {"braking_distance": 750, "displacement":1250,"emergency_braking_distance":20,"velocity":100,"acceleration":8}};  // TODO
   ASSERT_EQ(actualJson, expectedJson) << "Navigation json does not match expected output.";
   ASSERT_TRUE(writer.isValidJson()) << "Navigation json invalid.";
 }
@@ -149,7 +149,7 @@ TEST_F(WriterPackData, packsSensorsData)
   writer.packSensorsData();
   writer.end();
   const std::string actualJson   = writer.getString();
-  const std::string expectedJson = {[{"lp_battery":{"average_temp":int, "voltage":int,"current":int,"charge":int,"low_temp":int,"high_temp":int,"low_voltage_cell":int,"high_voltage_cell":int,"imd_fault": false}},{"lp_battery":{"average_temp":int, "voltage":int,"current":int,"charge":int,"low_temp":int,"high_temp":int,"low_voltage_cell":int,"high_voltage_cell":int,"imd_fault": false}}],[{"hp_battery":{"average_temp":int, "voltage":int,"current":int,"charge":int,"low_temp":int,"high_temp":int,"low_voltage_cell":int,"high_voltage_cell":int,"imd_fault": false}}], "brakes_retracted": bool,"temperature":int,"brake status": String,"sensors_status":bool, "batteries_status": bool};  // TODO
+  const std::string expectedJson = {"sensors": {[{"lp_battery":{"average_temp":int, "voltage":int,"current":int,"charge":int,"low_temp":int,"high_temp":int,"low_voltage_cell":int,"high_voltage_cell":int,"imd_fault": false}},{"lp_battery":{"average_temp":int, "voltage":int,"current":int,"charge":int,"low_temp":int,"high_temp":int,"low_voltage_cell":int,"high_voltage_cell":int,"imd_fault": false}}],[{"hp_battery":{"average_temp":int, "voltage":int,"current":int,"charge":int,"low_temp":int,"high_temp":int,"low_voltage_cell":int,"high_voltage_cell":int,"imd_fault": false}}], "brakes_retracted": bool,"temperature":int,"brake status": String,"sensors_status":bool, "batteries_status": bool}};  // TODO
   ASSERT_EQ(actualJson, expectedJson) << "Sensors json does not match expected output.";
   ASSERT_TRUE(writer.isValidJson()) << "Sensors json invalid.";
 }
@@ -162,7 +162,7 @@ TEST_F(WriterPackData, packsMotorData)
   writer.packMotorData();
   writer.end();
   const std::string actualJson   = writer.getString();
-  const std::string expectedJson = {"motor_rpms":[0,0,0,0],"motors_status":String} ;  // TODO
+  const std::string expectedJson = {"motors": {"motor_rpms":[0,0,0,0],"motors_status":String} ;  // TODO
   ASSERT_EQ(actualJson, expectedJson) << "Motor json does not match expected output.";
   ASSERT_TRUE(writer.isValidJson()) << "Motor json invalid.";
 }
@@ -175,7 +175,7 @@ TEST_F(WriterPackData, packsStateMachineData)
   writer.packStateMachineData();
   writer.end();
   const std::string actualJson   = writer.getString();
-  const std::string expectedJson = {"critical_failure": bool,"current_state": String};  // TODO
+  const std::string expectedJson = {"state_machine": {"critical_failure": bool,"current_state": String}};  // TODO
   ASSERT_EQ(actualJson, expectedJson) << "State machine json does not match expected output.";
   ASSERT_TRUE(writer.isValidJson()) << "State machine json invalid.";
 }

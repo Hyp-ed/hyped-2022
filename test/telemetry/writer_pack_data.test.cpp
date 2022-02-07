4a1b1afef723feda08d03ba0726e6ac25bbb74ef
adenhaus <adenrhaussmann@gmail.com>
2022-02-07 14:52:51 +0000

    moved CDS init from functions into tests

--- test/telemetry/writer_pack_data.test.cpp
+++ test/telemetry/writer_pack_data.test.cpp
@@ -16,7 +16,19 @@ struct WriterPackData : public ::testing::Test {
 
 Data &data_ = Data::getInstance();
 
-void initTelemetryData()
+TEST_F(WriterPackData, packsId)
+{
+  Writer writer(data_);
+  writer.start();
+  writer.packId(5);
+  writer.end();
+  const std::string actualJson   = writer.getString();
+  const std::string expectedJson = "{\"id\":5}";
+  ASSERT_EQ(actualJson, expectedJson) << "ID json does not match expected output.";
+  ASSERT_TRUE(writer.isValidJson()) << "ID json invalid.";
+}
+
+TEST_F(WriterPackData, packsTelemetryData)
 {
   Telemetry telemetry_data = data_.getTelemetryData();
 
@@ -29,9 +41,21 @@ void initTelemetryData()
   telemetry_data.module_status           = ModuleStatus::kCriticalFailure;
 
   data_.setTelemetryData(telemetry_data);
+
+  Writer writer(data_);
+  writer.start();
+  writer.packTelemetryData();
+  writer.end();
+  const std::string actualJson = writer.getString();
+  const std::string expectedJson
+    = "{\"telemetry\":{\"calibrate\":false,\"emergency_stop\":false,\"launch\":false,\"nominal_"
+      "breaking\":false,\"service_propulsion_go\":false,\"shutdown\":false,\"telemetry_status\":"
+      "\"CRITICAL_FAILURE\"}}";
+  ASSERT_EQ(actualJson, expectedJson) << "Telemetry json does not match expected output.";
+  ASSERT_TRUE(writer.isValidJson()) << "Telemetry json invalid.";
 }
 
-void initNavigationData()
+TEST_F(WriterPackData, packsNavigationData)
 {
   Navigation navigation_data = data_.getNavigationData();
 
@@ -43,9 +67,20 @@ void initNavigationData()
   navigation_data.module_status              = ModuleStatus::kReady;
 
   data_.setNavigationData(navigation_data);
+
+  Writer writer(data_);
+  writer.start();
+  writer.packNavigationData();
+  writer.end();
+  const std::string actualJson = writer.getString();
+  const std::string expectedJson
+    = "{\"navigation\":{\"braking_distance\":20,\"displacement\":100,\"emergency_braking_"
+      "distance\":50,\"velocity\":5,\"acceleration\":10,\"navigation_status\":\"READY\"}}";
+  ASSERT_EQ(actualJson, expectedJson) << "Navigation json does not match expected output.";
+  ASSERT_TRUE(writer.isValidJson()) << "Navigation json invalid.";
 }
 
-void initSensorsData()
+TEST_F(WriterPackData, packsSensorsData)
 {
   Sensors sensors_data        = data_.getSensorsData();
   Batteries batteries_data    = data_.getBatteriesData();
@@ -81,76 +116,7 @@ void initSensorsData()
   data_.setSensorsData(sensors_data);
   data_.setEmergencyBrakesData(brakes_data);
   data_.setBatteriesData(batteries_data);
-}
 
-void initMotorData()
-{
-  Motors motors_data = data_.getMotorData();
-
-  motors_data.module_status = ModuleStatus::kCriticalFailure;
-  for (int16_t i = 0; i < motors_data.kNumMotors; ++i) {
-    motors_data.rpms[i] = 150;
-  }
-
-  data_.setMotorData(motors_data);
-}
-
-void initStateMachineData()
-{
-  StateMachine state_machine_data = data_.getStateMachineData();
-
-  state_machine_data.critical_failure = false;
-  state_machine_data.current_state    = kAccelerating;
-
-  data_.setStateMachineData(state_machine_data);
-}
-
-TEST_F(WriterPackData, packsId)
-{
-  Writer writer(data_);
-  writer.start();
-  writer.packId(5);
-  writer.end();
-  const std::string actualJson   = writer.getString();
-  const std::string expectedJson = "{\"id\":5}";
-  ASSERT_EQ(actualJson, expectedJson) << "ID json does not match expected output.";
-  ASSERT_TRUE(writer.isValidJson()) << "ID json invalid.";
-}
-
-TEST_F(WriterPackData, packsTelemetryData)
-{
-  initTelemetryData();
-  Writer writer(data_);
-  writer.start();
-  writer.packTelemetryData();
-  writer.end();
-  const std::string actualJson = writer.getString();
-  const std::string expectedJson
-    = "{\"telemetry\":{\"calibrate\":false,\"emergency_stop\":false,\"launch\":false,\"nominal_"
-      "breaking\":false,\"service_propulsion_go\":false,\"shutdown\":false,\"telemetry_status\":"
-      "\"CRITICAL_FAILURE\"}}";
-  ASSERT_EQ(actualJson, expectedJson) << "Telemetry json does not match expected output.";
-  ASSERT_TRUE(writer.isValidJson()) << "Telemetry json invalid.";
-}
-
-TEST_F(WriterPackData, packsNavigationData)
-{
-  initNavigationData();
-  Writer writer(data_);
-  writer.start();
-  writer.packNavigationData();
-  writer.end();
-  const std::string actualJson = writer.getString();
-  const std::string expectedJson
-    = "{\"navigation\":{\"braking_distance\":20,\"displacement\":100,\"emergency_braking_"
-      "distance\":50,\"velocity\":5,\"acceleration\":10,\"navigation_status\":\"READY\"}}";
-  ASSERT_EQ(actualJson, expectedJson) << "Navigation json does not match expected output.";
-  ASSERT_TRUE(writer.isValidJson()) << "Navigation json invalid.";
-}
-
-TEST_F(WriterPackData, packsSensorsData)
-{
-  initSensorsData();
   Writer writer(data_);
   writer.start();
   writer.packSensorsData();
@@ -171,7 +137,15 @@ TEST_F(WriterPackData, packsSensorsData)
 
 TEST_F(WriterPackData, packsMotorData)
 {
-  initMotorData();
+  Motors motors_data = data_.getMotorData();
+
+  motors_data.module_status = ModuleStatus::kCriticalFailure;
+  for (int16_t i = 0; i < motors_data.kNumMotors; ++i) {
+    motors_data.rpms[i] = 150;
+  }
+
+  data_.setMotorData(motors_data);
+
   Writer writer(data_);
   writer.start();
   writer.packMotorData();
@@ -185,7 +159,13 @@ TEST_F(WriterPackData, packsMotorData)
 
 TEST_F(WriterPackData, packsStateMachineData)
 {
-  initStateMachineData();
+  StateMachine state_machine_data = data_.getStateMachineData();
+
+  state_machine_data.critical_failure = false;
+  state_machine_data.current_state    = kAccelerating;
+
+  data_.setStateMachineData(state_machine_data);
+
   Writer writer(data_);
   writer.start();
   writer.packStateMachineData();

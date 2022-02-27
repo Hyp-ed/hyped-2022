9041538728d38d3314e5bd58191f9ada377d12c8
Ishan Mishra <ishanmisra200@gmail.com>
2022-02-27 21:59:32 +0000

    sending via string and general cleanup

--- arduino/irsensor.cpp
+++ arduino/irsensor.cpp
@@ -1,57 +1,48 @@
+#define MAX_LENGTH 4   //one more for null char
 // Connects IR Sensors to pins
 int IRSensor1 = 2;
 int IRSensor2 = 3;
 int IRSensor3 = 4;
 int IRSensor4 = 5;
-// Connects output pins
-int Output1 = 13;
-int Output2 = 14;
-int Output3 = 15;
-int Output4 = 16;
+int wheelValues[MAX_LENGTH] = {0,0,0,0};
 
-void setup(){
-    // IRSensors are inputs
-    pinMode(IRSensor1, INPUT);
-    pinMode(IRSensor2, INPUT);
-    pinMode(IRSensor3, INPUT);
-    pinMode(IRSensor4, INPUT);
-    // Outputs
-    pinMode(Output1, OUTPUT);
-    pinMode(Output2, OUTPUT);
-    pinMode(Output3, OUTPUT);
-    pinMode(Output4, OUTPUT);
+void setup() {
+  Serial.begin(9600);
+
+  sendArray(wheelValues);
 }
-// Read IR Sensors + Output based on reading
-void loop(){
-    statusSensor1 = digitalRead(IRSensor1);
-    statusSensor2 = digitalRead(IRSensor2);
-    statusSensor3 = digitalRead(IRSensor3);
-    statusSensor4 = digitalRead(IRSensor4);
 
-    if (statusSensor1 == 1){
-        digitalWrite(Output1, LOW);
-    }
-    else {
-        digitalWrite(Output1, HIGH);
-    }
-    
-    if (statusSensor2 == 1){
-        digitalWrite(Output2, LOW);
-    }
-    else {
-        digitalWrite(Output2, HIGH);
-    }
-    
-    if (statusSensor3 == 1){
-        digitalWrite (Output3, LOW);
-    }
-    else {
-        digitalWrite (Output3, HIGH);
-    }
-    if (statusSensor4 == 1){
-        digitalWrite (Output4, LOW);
-    }
-    else {
-        digitalWrite (Output4, HIGH);
-    }
+void loop() {
+  int wheelValues[MAX_LENGTH] = {0,0,0,0}; 
+
+  if(digitalRead(IRSensor1)){
+    wheelValues[0]++;
+  }
+  if(digitalRead(IRSensor2)){
+    wheelValues[1]++;
+  }
+  if(digitalRead(IRSensor3)){
+    wheelValues[2]++;
+  }
+  if(digitalRead(IRSensor4)){
+    wheelValues[3]++;
+  }
+
+  sendArray(wheelValues);
+}
+
+void sendArray(int arr[]) 
+{
+
+  String msg;
+  for(int i = 0; i < sizeof(arr) - 1; i++) 
+  {
+    char number[arr[i] > 0 ? (int) log10 (arr[i]) + 1 : 1]; 
+    sprintf(number, "%d", arr[i]);
+    msg += number; 
+    msg += ","; 
+  }
+
+  Serial.println(msg);
+  
 }

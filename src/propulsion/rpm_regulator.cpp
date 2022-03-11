d83857fcd2785cdf8c41b8c4d19d4d30d2d7fb0e
TomLonergan03 <tomlonergan91@gmail.com>
2022-03-11 22:05:58 +0000

    Fix static declaration

--- src/propulsion/rpm_regulator.cpp
+++ src/propulsion/rpm_regulator.cpp
@@ -8,8 +8,7 @@ RpmRegulator::RpmRegulator()
 {
 }
 
-static int32_t RpmRegulator::calculateRpm(const data::nav_t actual_velocity,
-                                          const int32_t actual_rpm)
+int32_t RpmRegulator::calculateRpm(const data::nav_t actual_velocity, const int32_t actual_rpm)
 {
   const int32_t optimal_rpm = calculateOptimalRpm(actual_velocity);
   if (actual_rpm == optimal_rpm) { return actual_rpm; }
@@ -17,14 +16,14 @@ static int32_t RpmRegulator::calculateRpm(const data::nav_t actual_velocity,
   return std::max(target, 0);
 }
 
-static int32_t RpmRegulator::calculateOptimalRpm(const data::nav_t actual_velocity)
+int32_t RpmRegulator::calculateOptimalRpm(const data::nav_t actual_velocity)
 {
   // polynomial values from simulation
   return std::round(0.32047 * actual_velocity * actual_velocity + 297.72578 * actual_velocity
                     + 1024.30824);
 }
 
-static int32_t RpmRegulator::step(const int32_t optimal_rpm, const int32_t actual_rpm)
+int32_t RpmRegulator::step(const int32_t optimal_rpm, const int32_t actual_rpm)
 {
   if (actual_rpm < optimal_rpm) { return std::round(optimal_rpm * 0.1); }
   return std::round(optimal_rpm * -0.05);

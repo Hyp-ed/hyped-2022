02037ebf640b8dfc2fd949a87c95ef08f3c4a8ff
adenhaus <adenrhaussmann@gmail.com>
2022-03-21 18:51:00 +0000

    added fake pressure

---
+++ src/sensors/fake_pressure.cpp
@@ -0,0 +1,59 @@
+#include "fake_pressure.hpp"
+
+#include <stdlib.h>
+
+#include <utils/timer.hpp>
+
+namespace hyped::sensors {
+
+FakePressure::FakePressure(utils::Logger &log, bool is_fail)
+    : data_(data::Data::getInstance()),
+      log_(log),
+      failure_(300),
+      success_(30),
+      is_fail_(is_fail),
+      acc_start_time_(0),
+      acc_started_(false),
+      failure_time_(0),
+      failure_happened_(false)
+{
+  pressure_.pressure = success_;
+  if (is_fail_) {
+    log_.info("fail initialised");
+  } else {
+    log_.info("initialised");
+  }
+}
+
+void FakePressure::run()
+{
+  // We want to fail after we start accelerating
+  // We can make it random from 0 to 20 seconds
+  if (!acc_started_) {
+    data::State state = data_.getStateMachineData().current_state;
+    if (state == data::State::kAccelerating) {
+      acc_start_time_ = utils::Timer::getTimeMicros();
+      // Generate a random time for a failure
+      if (is_fail_) failure_time_ = (rand() % 20 + 1) * 1000000;
+      acc_started_ = true;
+    }
+  }
+  checkFailure();
+}
+
+void FakePressure::checkFailure()
+{
+  if (is_fail_ && failure_time_ != 0 && !failure_happened_) {
+    if (utils::Timer::getTimeMicros() - acc_start_time_ >= failure_time_) {
+      pressure_.pressure        = failure_;
+      failure_happened_ = true;
+    }
+  }
+}
+
+uint8_t FakePressure::getData()
+{
+  return pressure_.pressure;
+}
+
+}  // namespace hyped::sensors
---
+++ src/sensors/fake_pressure.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "interface.hpp"
+
+#include <string>
+
+#include <utils/logger.hpp>
+
+namespace hyped::sensors {
+
+class FakePressure : public ITemperature {
+ public:
+  /**
+   * @brief Construct a new Fake Temperature object
+   *
+   * @param log
+   * @param is_fail
+   */
+  FakePressure(utils::Logger &log, bool is_fail);
+
+  /**
+   * @brief returns int representation
+   *
+   * @return int pressure mbar
+   */
+  uint8_t getData() override;
+
+  /**
+   * @brief waits for acceleration, generate random time for failure
+   */
+  void run() override;
+
+ private:
+  data::Data &data_;
+  utils::Logger &log_;
+
+  /**
+   * @brief dependent on is_fail_, set to fail value
+   */
+  void checkFailure();
+
+  // values mbar
+  int failure_;
+  int success_;
+  data::PressureData pressure_;
+
+  bool is_fail_;
+
+  uint64_t acc_start_time_;
+  bool acc_started_;
+  uint64_t failure_time_;
+  bool failure_happened_;
+};
+}  // namespace hyped::sensors

b82bcd4d681aee27b6c9ce00a331c96283bdc34a
Kshitij Sharma <kshitij4sharma@gmail.com>
2022-03-31 02:00:02 +0100

    SNS: Implemented Fake Brake Pressure

---
+++ src/sensors/fake_brake_pressure.cpp
@@ -0,0 +1,60 @@
+#include "fake_brake_pressure.hpp"
+
+#include <stdlib.h>
+
+#include <utils/system.hpp>
+#include <utils/timer.hpp>
+
+namespace hyped::sensors {
+
+FakeBrakePressure::FakeBrakePressure(const bool is_fail)
+    : data_(data::Data::getInstance()),
+      log_("FAKE-BRAKE-PRESSURE", utils::System::getSystem().config_.log_level_sensors),
+      failure_(15),  // TODO: UPDATE ONCE FAIL VALUES ARE CONFIRMED
+      success_(10),  // TODO: UPDATE ONCE FAIL VALUES ARE CONFIRMED
+      is_fail_(is_fail),
+      acc_start_time_(0),
+      acc_started_(false),
+      failure_time_(0),
+      failure_happened_(false)
+{
+  pressure_data_.brake_pressure = success_;
+  if (is_fail_) {
+    log_.info("fail initialised");
+  } else {
+    log_.info("initialised");
+  }
+}
+
+void FakeBrakePressure::run()
+{
+  // We want to fail after we start accelerating
+  // We can make it random from 0 to 20 seconds
+  if (!acc_started_) {
+    data::State state = data_.getStateMachineData().current_state;
+    if (state == data::State::kAccelerating) {
+      acc_start_time_ = utils::Timer::getTimeMicros();
+      // Generate a random time for a failure
+      if (is_fail_) failure_time_ = (rand() % 20 + 1) * 1000000;
+      acc_started_ = true;
+    }
+  }
+  checkFailure();
+}
+
+void FakeBrakePressure::checkFailure()
+{
+  if (is_fail_ && failure_time_ != 0 && !failure_happened_) {
+    if (utils::Timer::getTimeMicros() - acc_start_time_ >= failure_time_) {
+      pressure_data_.brake_pressure = failure_;
+      failure_happened_             = true;
+    }
+  }
+}
+
+uint8_t FakeBrakePressure::getData() const
+{
+  return pressure_data_.brake_pressure;
+}
+
+}  // namespace hyped::sensors
---
+++ src/sensors/fake_brake_pressure.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "interface.hpp"
+
+#include <string>
+
+#include <utils/logger.hpp>
+
+namespace hyped::sensors {
+
+class FakeBrakePressure : public IBrakePressure {
+ public:
+  /**
+   * @brief Construct a new Fake Brake Pressure object
+   *
+   * @param log
+   * @param is_fail
+   */
+  FakeBrakePressure(const bool is_fail);
+
+  /**
+   * @brief returns int representation
+   *
+   * @return int pressure bars
+   */
+  uint8_t getData() const override;
+
+  /**
+   * @brief waits for acceleration, generate random time for failure
+   */
+  void run() override;
+
+ private:
+  data::Data &data_;
+  utils::Logger log_;
+
+  /**
+   * @brief dependent on is_fail_, set to fail value
+   */
+  void checkFailure();
+
+  // values bars
+  int failure_;
+  int success_;
+  data::BrakePressureData pressure_data_;
+
+  bool is_fail_;
+
+  uint64_t acc_start_time_;
+  bool acc_started_;
+  uint64_t failure_time_;
+  bool failure_happened_;
+};
+}  // namespace hyped::sensors
